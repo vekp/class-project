4bd62f6223863d7df9498c8beb0abeb7a4093aed
Vincent Ekpanyaskun <vekpanya@myune.edu.au>
2023-08-04 15:28:49 +1000

    Update Javadoc for AchievementUI constructor.

--- javaprojects/client/src/main/java/minigames/client/achievementui/AchievementUI.java
+++ javaprojects/client/src/main/java/minigames/client/achievementui/AchievementUI.java
@@ -1,6 +1,5 @@
 package minigames.client.achievementui;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -9,8 +8,8 @@ public class AchievementUI extends JPanel {
     public static final String TITLE = "Achievements";
 
     /**
-     * Creates a new <code>JPanel</code> with a double buffer
-     * and a flow layout.
+     * Creates a new JPanel containing a user interface for viewing achievements.
+     * @param returnAction an ActionListener for returning to the previous screen.
      */
     public AchievementUI(ActionListener returnAction) {
         this.setPreferredSize(new Dimension(800, 600));
