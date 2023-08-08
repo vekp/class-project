84e2479e45d2e5902a432df3020bf9183597fbca
Anonymized Author <anon@example.com>
2023-08-08 23:59:46 +1000

    Removed some unneeded code and added ability to create and display Cell arrays

--- javaprojects/server/src/main/java/minigames/server/battleship/Board.java
+++ javaprojects/server/src/main/java/minigames/server/battleship/Board.java
@@ -1,11 +1,14 @@
 package minigames.server.battleship;
 
+import static java.lang.Math.round;
+
 /**
  * The Board Class contains all the information about the current state of a player's game board, including the player's name
  */
 public class Board {
     private String playerName;
-    private String[][] grid; // A two-dimensional array of Strings for drawing the game board
+    private String boardTitle;
+    private Cell[][] grid; // A two-dimensional array of Cells for drawing the game board
     private Ship[] vessels = new Ship[5];  // An array containing each of the five ship types for the current board
     private int turnNumber;  // The current turn number
 
@@ -17,19 +20,7 @@ public class Board {
         this.playerName = playerName;
         this.turnNumber = 0;
         // Create a default grid
-        this.grid = new String[][]{
-            {" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"},
-            {"A", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"B", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"C", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"D", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"E", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"F", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"G", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"H", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"I", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
-            {"J", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"}
-        };
+        this.grid = defaultGridCreator();
     }
 
     
@@ -38,7 +29,7 @@ public class Board {
      * Return the 2D string array to display in the client window
      * @return The 2D String Array in its current state
      */
-    public String[][] getGrid(){return this.grid;}
+    public Cell[][] getGrid(){return this.grid;}
 
 
 
@@ -53,5 +44,51 @@ public class Board {
      * @return An int for the current turn number
      */
     public int getTurnNumber(){return this.turnNumber;}
+
+    /**
+     * Function to create a grid of strings to be displayed
+     * @param boardTitle
+     * @param grid
+     * @return
+     */
+    public static String createGrid(String boardTitle, Cell[][] grid) {
+        StringBuilder gridStrings = new StringBuilder();
+        String chars = "ABCDEFGHI";
+
+        // Character width of the board
+        int strLength = 22;
+        int titleSpace = round((strLength - boardTitle.length()) / 2);
+
+        // Adds space to the start of the title to centre the text
+        for (int i = 0; i < titleSpace; i++) {
+            gridStrings.append(" ");
+        }
+        gridStrings.append(boardTitle).append("\n").append(" ---------------------\n");
+
+        for (int i=0; i<10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i==0 && j==0) gridStrings.append("   ");
+                if (i==0) gridStrings.append(j).append(" ");
+                if (j==0 && i!=0) gridStrings.append(" ").append(chars.charAt(i-1)).append(" ");
+                if (i>0) gridStrings.append(grid[i][j].getCellType()).append(" ");
+            }
+            gridStrings.append("\n");
+        }
+        return gridStrings.toString();
+    }
+
+    /**
+     * Create a default grid of Cells
+     * @return 2D array of Cells
+     */
+    public Cell[][] defaultGridCreator() {
+        Cell[][] grid = new Cell[10][10];
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+        return grid;
+    }
 
 }
