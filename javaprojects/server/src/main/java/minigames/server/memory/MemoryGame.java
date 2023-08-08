37f306caff7c976bb53e16e8fc41c5c0731f1707
Anonymized Author <anon@example.com>
2023-08-08 17:47:02 +1000

    DRAFT - Add base code to MemoryGame.java & MemoryServer.java in server/memory (Update)

--- javaprojects/server/src/main/java/minigames/server/memory/MemoryGame.java
+++ javaprojects/server/src/main/java/minigames/server/memory/MemoryGame.java
@@ -1,5 +1,81 @@
 package minigames.server.memory;
 
+import io.vertx.core.json.JsonObject;
+import java.util.*;
+
+import minigames.commands.CommandPackage;
+import minigames.rendering.*;
+import minigames.rendering.NativeCommands.LoadClient;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Represents an actual Memory game in progress.
+ * Used and adapted MuddleGame.java
+ */
 public class MemoryGame {
-    
+
+    /** A logger for logging output */
+    private static final Logger logger = LogManager.getLogger(MemoryGame.class);
+
+    /** Uniquely identifies this game */
+    String name;
+
+    public MemoryGame(String name) {
+        this.name = name;
+    }
+
+    // Players
+    HashMap<String, MemoryPlayer> players = new HashMap<>();
+
+    /** Players currently playing this game */
+    public String[] getPlayerNames() {
+        return players.keySet().toArray(String[]::new);
+    }
+
+    /** Metadata for this game */
+    public GameMetadata gameMetadata() {
+        return new GameMetadata("Memory", name, getPlayerNames(), true);
+    }
+
+    public RenderingPackage runCommands(CommandPackage cp) {   
+        logger.info("Received command package {}", cp);     
+        //MemoryPlayer p = players.get(cp.player());
+
+        ArrayList<JsonObject> renderingCommands = new ArrayList<>();
+        JsonObject cmd = cp.commands().get(0);
+        switch (cmd.getString("command")) {
+            case "exitGame" -> {
+                players.remove(cp.player());
+                renderingCommands.add(new JsonObject().put("command", "showGames"));
+            }
+        }
+
+        return new RenderingPackage(this.gameMetadata(), renderingCommands);
+    }
+
+    /** Joins this game */
+    public RenderingPackage joinGame(String playerName) {
+        if (players.containsKey(playerName)) {
+            return new RenderingPackage(
+                gameMetadata(),
+                Arrays.stream(new RenderingCommand[] {
+                    new NativeCommands.ShowMenuError("That name's not available")
+                }).map((r) -> r.toJson()).toList()
+            );
+        } else {
+            MemoryPlayer p = new MemoryPlayer(playerName);
+            players.put(playerName, p);
+
+            ArrayList<JsonObject> renderingCommands = new ArrayList<>();
+            renderingCommands.add(new LoadClient("Memory", "Memory", name, playerName).toJson());
+
+            return new RenderingPackage(gameMetadata(), renderingCommands);
+        }
+    }
+
+    // Create a record of MemoryPlayer class
+    record MemoryPlayer(String name) {}
+
 }
--- javaprojects/server/src/main/java/minigames/server/memory/MemoryServer.java
+++ javaprojects/server/src/main/java/minigames/server/memory/MemoryServer.java
@@ -1,5 +1,74 @@
 package minigames.server.memory;
 
-public class MemoryServer {
+import io.vertx.core.Future;
+
+import java.util.HashMap;
+import java.util.Random;
+
+import minigames.commands.CommandPackage;
+import minigames.rendering.GameMetadata;
+import minigames.rendering.GameServerDetails;
+import minigames.rendering.RenderingPackage;
+import minigames.server.ClientType;
+import minigames.server.GameServer;
+
+/**
+ * Our MemoryServer holds MemoryGame. 
+ * When it receives a CommandPackage, it finds the MemoryGame and calls it.
+ * Used and adapted MuddleServer.java
+ */
+public class MemoryServer implements GameServer {
+
+    static final String chars = "abcdefghijklmopqrstuvwxyz";
+
+    /** Holds the game in progress in memory (no db). */
+    HashMap<String, MemoryGame> games = new HashMap<>();
+
+    /** Random name */
+    static String randomName() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 12; i++) {
+            sb.append(chars.charAt(r.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public GameServerDetails getDetails() {
+        return new GameServerDetails("Memory", "Find the pairs in the Memory card game!");
+    }
+
+    // Only supports Java Swing
+    @Override
+    public ClientType[] getSupportedClients() {
+        return new ClientType[] { ClientType.Swing };
+    }
+
+    @Override
+    public GameMetadata[] getGamesInProgress() {
+        return games.keySet().stream().map((name) -> {
+            return new GameMetadata("Memory", name, games.get(name).getPlayerNames(), true);
+        }).toArray(GameMetadata[]::new);
+    }
+
+    @Override
+    public Future<RenderingPackage> newGame(String playerName) {
+        MemoryGame g = new MemoryGame(randomName());
+        games.put(g.name, g);
+        return Future.succeededFuture(g.joinGame(playerName));
+    }
+
+    @Override
+    public Future<RenderingPackage> joinGame(String game, String playerName) {
+        MemoryGame g = games.get(game);
+        return Future.succeededFuture(g.joinGame(playerName));
+    }
+
+    @Override
+    public Future<RenderingPackage> callGame(CommandPackage cp) {
+        MemoryGame g = games.get(cp.gameId());
+        return Future.succeededFuture(g.runCommands(cp));
+    }
     
 }
