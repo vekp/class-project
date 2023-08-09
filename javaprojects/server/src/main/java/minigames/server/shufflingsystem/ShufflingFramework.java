0a33cd0588aa8bf498cdf0181aef234b5fd5a906
Anonymized Author <anon@example.com>
2023-08-09 20:04:59 +1000

    Fix typo | shufle() -> shuffle()

--- javaprojects/server/src/main/java/minigames/server/shufflingsystem/ShufflingFramework.java
+++ javaprojects/server/src/main/java/minigames/server/shufflingsystem/ShufflingFramework.java
@@ -13,10 +13,12 @@ public class ShufflingFramework<T> {
      * This method will modify the original array so ensure you have a copy of
      * your original array, or an easy way to create a new unshuffled version
      */
-    public static <T> void shufle(T[] items){
+    public static <T> void shuffle(T[] items){
         //We will need to implement a playing card class, I have a basic one if you'd like to look at it Melinda.
         //It's early days so play around with the code, it can definitely be better than what I've done here.
         for(int i = items.length -1; i > 0; i--){
+            //Adding +1 to i because we DO want to potentially swap an item with itself.
+            //Otherwise this would be an implementation of Satollo's algorithm.
             int swap = random.nextInt(i + 1);
             T temp = items[i];
             items[i] = items[swap];
