200fa01c56e876a3768024315f2abb3d3ff81a83
Anonymized Author <anon@example.com>
2023-08-09 20:01:25 +1000

    Shuffling system shuffle() method is now static. The method now uses a generic data type and could be used to shuffle or randomise any array using the Fisher-Yates shuffle

--- javaprojects/server/src/main/java/minigames/server/shufflingsystem/ShufflingFramework.java
+++ javaprojects/server/src/main/java/minigames/server/shufflingsystem/ShufflingFramework.java
@@ -1,31 +1,27 @@
 package minigames.server.shufflingsystem;
 
-import minigames.server.solitaireOrMemory.DeckOfCards.PlayingCard;
 import java.util.Random;
 
 
-public class ShufflingFramework {
+public class ShufflingFramework<T> {
 
+    private static Random random = new Random();
         /**
-     * This method takes an array of playing card objects and performs 
+     * This method takes an array of any data type 
      * the fisher-yates in place shuffling algorithm on them. 
-     * @param cards -> an array of playingCard objects
+     * @param items -> an array of items to be shuffled
      * This method will modify the original array so ensure you have a copy of
      * your original array, or an easy way to create a new unshuffled version
-     * If you want to use this to randomise objects in your game, I would recommend
-     * overwriting the method, I'll place a commented function interface below. 
      */
-    public PlayingCard[] shufle(PlayingCard[] cards){
+    public static <T> void shufle(T[] items){
         //We will need to implement a playing card class, I have a basic one if you'd like to look at it Melinda.
         //It's early days so play around with the code, it can definitely be better than what I've done here.
-        Random rand = new Random();
-        for(int i = cards.length -1; i > 0; i--){
-            int swap = rand.nextInt(i + 1);
-            PlayingCard temp = cards[i];
-            cards[i] = cards[swap];
-            cards[swap] = temp;
+        for(int i = items.length -1; i > 0; i--){
+            int swap = random.nextInt(i + 1);
+            T temp = items[i];
+            items[i] = items[swap];
+            items[swap] = temp;
         } 
-        return cards;
     }
     
 }
