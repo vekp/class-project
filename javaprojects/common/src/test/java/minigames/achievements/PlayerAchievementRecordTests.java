ccdb2460fa6a9826a871f5146de46c5b2ece1bea
Anonymized Author <anon@example.com>
2023-08-10 08:43:21 +1000

    Updated tests for PlayerAchievementRecord

--- javaprojects/common/src/test/java/minigames/achievements/PlayerAchievementRecordTests.java
+++ javaprojects/common/src/test/java/minigames/achievements/PlayerAchievementRecordTests.java
@@ -1,7 +1,7 @@
 package minigames.achievements;
 
 import java.util.*;
-import io.vertx.core.json.JsonObject;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,8 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class PlayerAchievementRecordTests {
 
+    /** Checks that an entire PlayerAchievementRecord will be accurately converted to and from a JSON string */
     @Test
-    public void constructsPackageFromJSON(){
+    public void playerRecordFromJSON(){
+        //Some random test data to fill out the record
         List<Achievement> list1 = new ArrayList<>();
         List<Achievement> list2 = new ArrayList<>();
         list1.add(new Achievement("Ach 1", "My Achievement", "None", false));
@@ -28,14 +30,55 @@ public class PlayerAchievementRecordTests {
         gameList.add(new GameAchievementState("Game 2", list2, list1));
 
         PlayerAchievementRecord playerRecord = new PlayerAchievementRecord("James", gameList);
-        String jsonString = playerRecord.toJSON();
 
+        //convert to JSON and back. Once reconstructed, the record should have the same data in the same spots
+        //as we put in above.
+        String jsonString = playerRecord.toJSON();
         PlayerAchievementRecord afterJSON = PlayerAchievementRecord.fromJSON(jsonString);
 
+        //this is the only primitive data in the player record, it should match the name we put in
         assertEquals(afterJSON.playerID(), "James");
+
+        //these should be equal if 1 -the below conversion tests pass (individual components convert properly)
+        //and 2 - the player record is properly using the component conversion methods
         assertEquals(afterJSON.gameAchievements().get(0).gameID(), "Game 1");
         assertEquals(afterJSON.gameAchievements().get(1).gameID(), "Game 2");
-        //2nd locked achievement for the 2nd game should be 'Achieve 1'
         assertEquals(afterJSON.gameAchievements().get(1).locked().get(1).name(), "Achieve 1");
+    }
+
+    //The below check individual JSON conversion for the classes that are fields of PlayerAchievementRecord
+
+    /** Testing that GameAchievementState converts properly to/from json */
+    @Test
+    public void gameAchievementStateFromJSON(){
+        List<Achievement> list1 = new ArrayList<>();
+        List<Achievement> list2 = new ArrayList<>();
+        list1.add(new Achievement("Ach 1", "My Achievement", "None", false));
+        list1.add(new Achievement("Achieve 1", "My Other Achievement", "None", false));
+        list2.add(new Achievement("Achievement 2", "My Also another Achievement", "None", false));
+        list2.add(new Achievement("Ach 2", "My First Achievement", "None", false));
+        list2.add(new Achievement("Achieve 2", "My Second Achievement", "None", false));
+
+        GameAchievementState state = new GameAchievementState("Test Game ABCD", list1, list2);
+
+        String json = state.toJSON();
+        GameAchievementState fromJSON = GameAchievementState.fromJSON(json);
+
+        assertEquals(fromJSON.gameID(), "Test Game ABCD");
+        assertEquals(fromJSON.unlocked().get(0).name(), "Ach 1");
+        assertEquals(fromJSON.locked().get(2).name(), "Achieve 2");
+    }
+
+    /** Testing the conversion of Achievement data to/from JSON */
+    @Test
+    public void achievementFromJSON(){
+        Achievement testAchievement = new Achievement("Tadpole", "ABCD", "", true);
+
+        String json = testAchievement.toJSON();
+        Achievement fromJSON = Achievement.fromJSON(json);
+
+        assertEquals(fromJSON.name(), "Tadpole");
+        assertEquals(fromJSON.description(), "ABCD");
+        assertTrue(fromJSON.hidden());
     }
 }
