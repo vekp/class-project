b775226499b73971c483d0c4d8ee1876ae15e40b
Anonymized Author <anon@example.com>
2023-08-10 21:07:33 +1000

    DRAFT - Add base code to Memory.java in client/memory (Update)

--- javaprojects/client/src/main/java/minigames/client/memory/Memory.java
+++ javaprojects/client/src/main/java/minigames/client/memory/Memory.java
@@ -1,5 +1,98 @@
 package minigames.client.memory;
 
-public class Memory {
+import io.vertx.core.json.JsonObject;
+
+import minigames.client.GameClient;
+import minigames.client.MinigameNetworkClient;
+import minigames.commands.CommandPackage;
+import minigames.rendering.GameMetadata;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+import org.apache.logging.log4j.*;
+
+public class Memory implements GameClient {
+
+    // Client instance
+    MinigameNetworkClient mnClient;
+
+    // Send commands to server
+    GameMetadata gm;
+
+    // Player
+    String player;
+
+    JPanel gamePanel;
+    JPanel headingPanel;
+    JLabel title;
+    JPanel playerPanel;
+    JLabel playerName;
+    JPanel gameOptionsPanel;
+    JButton restartLevelButton;
+    JButton restartGameButton;
+    JButton exitButton;
+    JPanel commandPanel;
+
+    JTextArea textArea; 
+
+    // Game variable
+    boolean gameStarted = false;
+
+    public Memory() {
+        // TODO
+    }
+
+    /** 
+     * Sends a command to the game at the server.
+     * This being a text adventure, all our commands are just plain text strings our gameserver will interpret.
+     * We're sending these as 
+     * { "command": command }
+     */
+    public void sendCommand(String command) {
+        JsonObject json = new JsonObject().put("command", command);
+
+        // Collections.singletonList() is a quick way of getting a "list of one item"
+        mnClient.send(new CommandPackage(gm.gameServer(), gm.name(), player, Collections.singletonList(json)));
+    }
+ 
+
+    /**
+     * What we do when our client is loaded into the main screen
+     */
+    @Override
+    public void load(MinigameNetworkClient mnClient, GameMetadata game, String player) {
+        this.mnClient = mnClient;
+        this.gm = game;
+        this.player = player;
+
+        // Add our components to the north, south, east, west, or centre of the main window's BorderLayout
+        mnClient.getMainWindow().addCenter(gamePanel);
+        mnClient.getMainWindow().addSouth(commandPanel);   
+
+        textArea.append("Starting...");
+
+        // Don't forget to call pack - it triggers the window to resize and repaint itself
+        mnClient.getMainWindow().pack();     
+    }
+
+    @Override
+    public void execute(GameMetadata game, JsonObject command) {
+        this.gm = game;
+
+        // We should only be receiving messages that our game understands
+        // Note that this uses the -> version of case statements, not the : version
+        // (which means we don't need to say "break;" at the end of our cases)
+        switch (command.getString("command")) {
+            case "clearText" -> textArea.setText("");
+            case "appendText" -> textArea.setText(textArea.getText() + command.getString("text"));
+        }
+    }
+
+    @Override
+    public void closeGame() {
+        // Nothing to do        
+    }
     
 }
