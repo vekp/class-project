f97683feecae2619a6759b17bab60f7cc3d2e966
Anonymized Author <anon@example.com>
2023-08-11 15:14:20 +0800

    Registered Telepathy with the game registery

--- javaprojects/server/src/main/java/minigames/server/Main.java
+++ javaprojects/server/src/main/java/minigames/server/Main.java
@@ -3,6 +3,7 @@ package minigames.server;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import minigames.server.muddle.MuddleServer;
+import minigames.server.telepathy.TelepathyServer;
 import io.vertx.core.Launcher;
 
 import org.apache.logging.log4j.LogManager;
@@ -18,36 +19,39 @@ public class Main extends AbstractVerticle {
     /** A logger for logging output */
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    /** 
+    /**
      * Port to start the server on.
-     * This is a little hacky, but when we run the server, we just put the port argument into this static field
+     * This is a little hacky, but when we run the server, we just put the port
+     * argument into this static field
      * so that the GameServer's start method can pick it up later
      */
     public static int port = 8080;
 
-    /** 
-     * The games that are available for each client. Static so that game servers and Main gan register them
+    /**
+     * The games that are available for each client. Static so that game servers and
+     * Main gan register them
      * without needing to worry about whether the server has started yet.
      */
     public static final GameRegistry gameRegistry = new GameRegistry();
 
     /**
-     * A place for groups to put code that registers their GameServer with the GameRegistry, etc.
+     * A place for groups to put code that registers their GameServer with the
+     * GameRegistry, etc.
      */
     private static void doWiring() {
         // Register our first demo game
         gameRegistry.registerGameServer("Muddle", new MuddleServer());
-
+        gameRegistry.registerGameServer("Telepathy", new TelepathyServer());
     }
 
     public static void main(String... args) {
         if (args.length > 0) {
             try {
-                int p = Integer.parseInt(args[0]);                
+                int p = Integer.parseInt(args[0]);
                 if (p >= 1024 && p <= 49151) {
                     port = p;
                 } else {
-                    logger.error("Port {} is outside the user port range of 1024 to 49151", args[0]);    
+                    logger.error("Port {} is outside the user port range of 1024 to 49151", args[0]);
                 }
             } catch (NumberFormatException ex) {
                 logger.error("Port {} could not be parsed as a number", args[0]);
@@ -58,7 +62,8 @@ public class Main extends AbstractVerticle {
         doWiring();
 
         // Ask the Vertx launcher to launch our "Verticle".
-        // This will cause Vert.x to start itself up, and then create a Main object and call our Main::start method
+        // This will cause Vert.x to start itself up, and then create a Main object and
+        // call our Main::start method
         logger.info("About to launch the server");
         Launcher.executeCommand("run", "minigames.server.Main");
     }
@@ -74,6 +79,5 @@ public class Main extends AbstractVerticle {
         gameServer = new MinigameNetworkServer(vertx);
         gameServer.start(port);
     }
-
 
 }
