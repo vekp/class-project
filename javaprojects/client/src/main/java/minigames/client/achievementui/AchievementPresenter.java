e3fd1564bfe21069a7d8c51966d90839fb897ccf
Vincent Ekpanyaskun <vekpanya@myune.edu.au>
2023-08-11 12:11:57 +1000

    Create large achievement panel method. Replace click action with message dialog showing large panel. Add additional image for hidden locked items.

--- javaprojects/client/src/main/java/minigames/client/achievementui/AchievementPresenter.java
+++ javaprojects/client/src/main/java/minigames/client/achievementui/AchievementPresenter.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import java.util.Arrays;
 import java.util.Random;
 
 public class AchievementPresenter {
@@ -25,7 +24,8 @@ public class AchievementPresenter {
 
     private ImageIcon makeScaledImage(int size) {
         // Attempt to set path of image
-        String path = achievementImageFolderLocation + achievement.mediaFileName().toLowerCase().replace(" ", "") + ".png";
+        String path = (achievement.hidden() && !isUnlocked) ? achievementImageFolderLocation + "mystery.png" :
+                achievementImageFolderLocation + achievement.mediaFileName().toLowerCase().replace(" ", "") + ".png";
         System.out.println(path);
         if (!new File(path).exists()) {
             path = path.replace(".png", ".jpg");
@@ -51,22 +51,15 @@ public class AchievementPresenter {
         Font boldFont = new Font(currentFont.getFontName(), Font.BOLD, currentFont.getSize());
         name.setFont(boldFont);
 
-        JTextArea description = new JTextArea(achievement.description());
-        description.setEditable(false);
-        description.setFocusable(false);
-        description.setWrapStyleWord(true);
-        description.setLineWrap(true);
-        description.setBackground(name.getBackground());
+        // Todo: make description a jtextpane.  Jtextarea can't be clicked on.
+        JLabel description = new JLabel(achievement.description());
+        description.setVerticalAlignment(JLabel.TOP);
 //        description.setBorder(new LineBorder(Color.BLACK));
 
         JPanel panel = new JPanel();
 //        panel.setBorder(new LineBorder(Color.BLACK));
-        JPanel imageAndSpacer = new JPanel();
         JLabel image = new JLabel(makeScaledImage(60));
-        imageAndSpacer.add(image);
-        Component spacer = Box.createRigidArea(new Dimension(115, 0));
-        imageAndSpacer.add(spacer);
-        panel.add(imageAndSpacer);
+        panel.add(image);
         JPanel textPanel = new JPanel(new BorderLayout());
         textPanel.add(name, BorderLayout.NORTH);
         textPanel.add(description);
@@ -75,31 +68,45 @@ public class AchievementPresenter {
             name.setForeground(Color.LIGHT_GRAY);
             description.setForeground(Color.LIGHT_GRAY);
             if (achievement.hidden()) {
-                panel.setBackground(Color.ORANGE);
                 description.setText(makeRandomText(achievement.description()));
             }
 
         } else if (achievement.hidden()) panel.setBackground(Color.YELLOW);
 
-        // Expand image if clicked on.
+        // Show large panel if clicked on.
         if (isUnlocked) {
             panel.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
-                    image.setIcon(makeScaledImage(180));
-                    imageAndSpacer.remove(spacer);
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    image.setIcon(makeScaledImage(60));
-                    if (!Arrays.stream(imageAndSpacer.getComponents()).toList().contains(spacer)) imageAndSpacer.add(spacer);
+                    JOptionPane.showMessageDialog(SwingUtilities.getWindowAncestor(panel), largeAchievementPanel(),
+                            "Your achievement", JOptionPane.PLAIN_MESSAGE);
                 }
             });
         }
         return panel;
     }
 
+    public JPanel largeAchievementPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel image = new JLabel(makeScaledImage(200));
+        image.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(image);
+
+        JLabel name = new JLabel(achievement.name());
+        Font nameFont = new Font(name.getFont().getFontName(), Font.BOLD, 36);
+        name.setFont(nameFont);
+        name.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(name);
+
+        JLabel description = new JLabel(achievement.description());
+        Font descriptionFont = new Font(description.getFont().getFontName(), Font.PLAIN, 20);
+        description.setFont(descriptionFont);
+        description.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(description);
+
+        return panel;
+    }
     private String makeRandomText(String text) {
         StringBuilder randomText = new StringBuilder();
         Random random = new Random();
