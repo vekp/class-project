845e66b0c5e2d91087f818577f6df04bc4a760bf
Anonymized Author <anon@example.com>
2023-08-12 17:19:43 +1000

    Add in buttons (buttons dont do anything yet)

--- javaprojects/client/src/main/java/minigames/client/peggle/Peggle.java
+++ javaprojects/client/src/main/java/minigames/client/peggle/Peggle.java
@@ -13,11 +13,6 @@ public class Peggle {
     private static final Color mainColour = new Color(1, 172, 252);
     private static final Color accentColour = new Color(255, 255, 255);
 
-
-
-
-
-
     public static void main(String[] args) {
 
 
@@ -32,10 +27,33 @@ public class Peggle {
         mainWindow.setIconImage(icon.getImage());
 
         //Initialise game with start screen
-        titleScreen = new JPanel();
+        titleScreen = displayTitleScreen();
         mainWindow.setContentPane(titleScreen);
         mainWindow.pack();
         mainWindow.setVisible(true);
 
     }
+
+
+    public static JPanel displayTitleScreen(){
+
+        //Basic properties of parent JPanel
+        titleScreen = new JPanel(new GridLayout(0,1,30,30));
+        titleScreen.setBackground(mainColour);
+
+        //Get UI details from UI class depending on response
+        JButton startGameButton = new JButton("Start Game");
+        JButton settingsButton = new JButton("Settings (In Development)");
+        JButton leaderboardButton = new JButton("Leaderboard (In Development)");
+
+        titleScreen.add(startGameButton);
+        titleScreen.add(settingsButton);
+        titleScreen.add(leaderboardButton);
+
+        return titleScreen;
+
+
+    }
+
+
 }
