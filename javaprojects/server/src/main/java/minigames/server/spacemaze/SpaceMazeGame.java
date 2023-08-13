cb80ec787070ce490056d700bbd657bfff757d12
Anonymized Author <anon@example.com>
2023-08-13 11:51:40 +1000

    Added key input to runCommands

--- javaprojects/server/src/main/java/minigames/server/spacemaze/SpaceMazeGame.java
+++ javaprojects/server/src/main/java/minigames/server/spacemaze/SpaceMazeGame.java
@@ -20,7 +20,7 @@ public class SpaceMazeGame {
     /** A logger for logging output */
     private static final Logger logger = LogManager.getLogger(SpaceMazeGame.class);
 
-    record SpaceMazePlayer (
+    record SpacePlayer (
             String name
     ) {
     }
@@ -28,9 +28,20 @@ public class SpaceMazeGame {
     /** String to uniquely identify this game*/
     String name;
 
-    public SpaceMazeGame(String name) {this.name = name;}
+    // The instance of MazeControl for this individual game
+    MazeControl mazeControl;
 
-    HashMap<String, SpaceMazePlayer> players = new HashMap<>();
+    /**
+     * Constructor
+     * @param name to identify the individual game
+     */
+    public SpaceMazeGame(String name) {
+        this.name = name;
+        this.mazeControl = new MazeControl();
+    }
+
+    // Players in this game
+    HashMap<String, SpacePlayer> players = new HashMap<>();
 
 
     /** The players currently playing this game */
@@ -45,15 +56,61 @@ public class SpaceMazeGame {
 
     public RenderingPackage runCommands(CommandPackage cp) {
         logger.info("Received command package {}", cp);
-        SpaceMazePlayer p = players.get(cp.player());
+        SpacePlayer p = players.get(cp.player());
 
         ArrayList<JsonObject> renderingCommands = new ArrayList<>();
+        String commandString = cp.commands().get(0).getValue("command");
 
-        //TODO Any to be recieved by our server
+        if (commandString.startsWith("key")) {
+            String keyPressed = commandString.toLowerCase.replace("key", "");
+            processKeyInput(keyPressed, p);
+        }
 
-        // quitToMenu() in common/rendering/NativeCommands.java
+        //TODO Any other to be recieved by our server
+
+        // Do we use quitToMenu() in common/rendering/NativeCommands.java?
 
         return new RenderingPackage(this.gameMetadata(), renderingCommands);
+    }
+
+    /**
+     * Method to process the key inputs sent by the client
+     * @param keyPressed a string representing the key that was pressed
+     * @param player the player that sent the key
+     */
+    private void processKeyInput(String keyPressed, SpacePlayer player) {
+        // Very verbose for now but it will get refactored a lot as the game develops
+        // validMove is essentailly being called twice, But we need to check the move is valid first
+        int[] pLoc = player.getLocation();
+        int x = pLoc[0];
+        int y = pLoc[1];
+
+        switch (keyPressed) {
+            case "up" -> {
+                if (mazeControl.validMove(x, y-1) {
+                    player.updateLocation(x, y-1);
+                    mazeControl.updateLocation(x, y-1);
+                }
+            }
+            case "down" -> if {
+                (mazeControl.validMove(x, y+1) {
+                    player.updateLocation(x, y+1);
+                    mazeControl.updateLocation(x, y+1);
+                }
+            }
+            case "left" -> {
+                if (mazeControl.validMove(x-1, y) {
+                    player.updateLocation(x-1, y);
+                    mazeControl.updateLocation(x-1, y);
+                }
+            }
+            case "right" -> {
+                if (mazeControl.validMove(x+1, y) {
+                    player.updateLocation(x+1, y);
+                    mazeControl.updateLocation(x+1, y);
+                }
+            }
+        }
     }
 
     /** Joins this game */
@@ -67,13 +124,13 @@ public class SpaceMazeGame {
                     }).map((r) -> r.toJson()).toList()
             );
         } else {
-            //Create the new player with starting position
-            SpaceMazePlayer p = new SpaceMazePlayer(playerName);
+            // Create the new player with starting position
+            SpacePlayer p = new SpacePlayer(playerName);
             players.put(playerName, p);
 
             ArrayList<JsonObject> renderingCommands = new ArrayList<>();
 
-            //TODO Any commands when a second player joins
+            // TODO Any commands when a second player joins
 
             renderingCommands.add(new LoadClient("SpaceMazeGame", "SpaceMaze", name, playerName).toJson());
             return new RenderingPackage(gameMetadata(), renderingCommands);
