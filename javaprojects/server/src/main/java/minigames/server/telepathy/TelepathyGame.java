92fd69371d51bf26f4f42c716ab2327479fca8d8
Anonymized Author <anon@example.com>
2023-08-15 10:12:43 +0800

    Changed players from HashSet to array of Player record
    Changed the way that players are stored within the TelepathyGame
    class to allow for keeping extra information related to that player
    in the future.
    
    When a player attempts to join a game verification is in place to
    check there is a spot available and their name is not already taken.

--- javaprojects/server/src/main/java/minigames/server/telepathy/TelepathyGame.java
+++ javaprojects/server/src/main/java/minigames/server/telepathy/TelepathyGame.java
@@ -3,28 +3,39 @@ package minigames.server.telepathy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import io.vertx.core.json.JsonObject;
 import minigames.commands.CommandPackage;
 import minigames.rendering.GameMetadata;
 import minigames.rendering.RenderingPackage;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TelepathyGame {
 
     // Logs output
     private static final Logger logger = LogManager.getLogger(TelepathyGame.class);
 
-    private HashSet<String> players = new HashSet<>();
+    
     private String name;
 
+    // Can have other fields later - chosen board piece, etc...
+    public record Player(
+        String name
+    ){}
+
+    private Player players[] =  {null, null};
 
     /**
      * Constructs a new game of Telepathy with a given name.
      * 
-     * @param name: String with the name to use for thisgame.
+     * @param name: String with the name to use for this game.
      */
     public TelepathyGame(String name) {
         this.name = name;
+        
+        this.players[0] = new Player("Empty");
+        this.players[1] = new Player("Empty");
     }
 
     /**
@@ -34,8 +45,8 @@ public class TelepathyGame {
      *         game can be joined.
      */
     public GameMetadata telepathyGameMetadata() {
-
-        return new GameMetadata("Telepathy", name, this.players.toArray(new String[this.players.size()]), true);
+        String[] playerNames = {players[0].name, players[1].name};
+        return new GameMetadata("Telepathy", name, playerNames, true);
     }
 
     public RenderingPackage runCommands(CommandPackage commandPackage) {
@@ -57,11 +68,26 @@ public class TelepathyGame {
      * @return RenderingPackage with instructions for the client.
      */
     public RenderingPackage joinGame(String name) {
-        // Need to add verification for player name on the server (duplicates, etc)
-        players.add(name);
+        ArrayList<JsonObject> renderingCommands = new ArrayList<>();
 
+        // Need to add verification for player name on the server (duplicates, etc)
+        if(this.players[0].name.equals("Empty")){
+            this.players[0] = new Player(name);
+            renderingCommands.add(new JsonObject().put("command", "joinedGameSuccess"));
+        } else{
+            if(this.players[0].name.equals(name)){
+                // Name taken
+                renderingCommands.add(new JsonObject().put("command", "joinedGameFail").put("message", "Unable to join game, name has already been taken."));
+            }
+            else if(!this.players[1].name.equals("Empty")){ 
+            // Spot already taken
+            renderingCommands.add(new JsonObject().put("command", "joinedGameFail").put("message", "Unable to join game, no spots available."));
+            }
+        }
+        
         // TODO Create rendering commands for the package
-        return new RenderingPackage(this.telepathyGameMetadata(), null);
+        
+        return new RenderingPackage(this.telepathyGameMetadata(), renderingCommands);
     }
 
     // Accessor methods
@@ -70,8 +96,8 @@ public class TelepathyGame {
      * Getter for current players in the game.
      * @return copy of the HashSet of players.
      */
-    public HashSet<String> getPlayers(){
-        return new HashSet<>(this.players);
+    public Player[] getPlayers(){
+        return Arrays.stream(this.players).toArray(Player[]::new);
     }
 
     /**
