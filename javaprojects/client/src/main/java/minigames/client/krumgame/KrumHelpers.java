c1c1d06bb696b0ccaf256d877c7b1facb8018790
Anonymized Author <anon@example.com>
2023-08-19 22:59:24 +1000

    Updated KrumHelper with readSprite static method
    This method is for reading sprites

--- javaprojects/client/src/main/java/minigames/client/krumgame/KrumHelpers.java
+++ javaprojects/client/src/main/java/minigames/client/krumgame/KrumHelpers.java
@@ -1,5 +1,10 @@
 package minigames.client.krumgame;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class KrumHelpers {
     static double distanceBetween(double ax, double ay, double bx, double by) {
         return Math.sqrt((ax - bx)*(ax-bx)+(ay-by)*(ay-by));
@@ -20,6 +25,26 @@ public class KrumHelpers {
         double magR = Math.sqrt(xa*xa + ya*ya);
         double result[] = {dirR, magR};
         return result;
+    }
+    /**
+     * This method reads an image file if fails to read then
+     * exit the program. 
+     * 
+     * @params: String fileName: image file name
+     * @return: BufferedImage
+     */
+    static BufferedImage readSprite(String fileName){
+        File spriteFile = new File(KrumC.imgDir + fileName);
+
+        try{
+            return ImageIO.read(spriteFile);
+        }catch (IOException e){
+            System.out.println("error reading sprite image" + fileName);
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+        }
     }
 
 }
