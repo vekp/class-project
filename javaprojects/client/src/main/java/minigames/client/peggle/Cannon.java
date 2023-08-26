2b11694a3d1f327f0add41cb1fc4f2699aef4dd2
Anonymized Author <anon@example.com>
2023-08-26 14:53:50 +1000

    Reverted InGameUI and Cannon changes back due to issues using assets, can re-implement once basic game prototype is working

--- javaprojects/client/src/main/java/minigames/client/peggle/Cannon.java
+++ javaprojects/client/src/main/java/minigames/client/peggle/Cannon.java
@@ -1,76 +1,32 @@
 package minigames.client.peggle;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.awt.Color;
 
-/*
- * Represents a cannon that can be rotated and drawn on the screen.
- */
 public class Cannon {
-    int x, y;  // Position of the cannon
-    double angle; // Angle at which the cannon is pointing
-    private BufferedImage cannonImage; //Image of the cannon
+    int x, y;  // position of the cannon
+    double angle; // angle of the cannon
 
-    /*
-     * Constructor that initializes the cannon with the specified position.
-     *
-     * @param x The x-coordinate of the cannon's position.
-     * @param y The y-coordinate of the cannon's position.
-     */
     public Cannon(int x, int y) {
         this.x = x;
         this.y = y;
         this.angle = 0;
-        try {
-            this.cannonImage = ImageIO.read(new File("./javaprojects/client/src/main/java/minigames/client/peggle/assets/objects/cannon02.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    /*
-     * Sets the angle of the cannon to the specified value (in radians),
-     * limited to a range of 30 to 150 degrees.
-     *
-     * @param angle The angle to set the cannon to, in radians.
-     */
     public void setAngle(double angle) {
-        // Convert degrees to radians
-        double minAngle = Math.toRadians(30); // 30 degrees
-        double maxAngle = Math.toRadians(150);  // 150 degrees
-
-        // Limit the angle to the specified range
-        this.angle = Math.max(minAngle, Math.min(maxAngle, angle));
+        this.angle = angle;
     }
 
-    /*
-     * Draws the cannon on the screen with scaling and rotation.
-     *
-     * @param g The graphics object used for drawing.
-     */
     public void draw(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+        int length = 70;  // Length of the cannon
+        int endX = x + (int)(length * Math.cos(angle));
+        int endY = y + (int)(length * Math.sin(angle));
 
-        // Draw the cannon image with scaling and rotation
-        AffineTransform at = new AffineTransform();
-        double scale = 0.4; // Change this to the desired scale factor
-        at.scale(scale, scale);
-        at.translate(x / scale, (y - cannonImage.getHeight(null) / 2) / scale);
-        at.rotate(angle + Math.PI / 2, 0, cannonImage.getHeight(null) / 2);
-        at.translate(-cannonImage.getWidth(null) / 2, -cannonImage.getHeight(null) / 2);
-        g2d.drawImage(cannonImage, at, null);
+        g.setColor(Color.RED);
+        g.drawLine(x, y, endX, endY);
     }
 
-    /*
-     * Sets the x-coordinate of the cannon's position.
-     *
-     * @param x The new x-coordinate for the cannon's position.
-     */
+    //To center the cannon based on window width
     public void setX(int x) {
         this.x = x;
     }
--- javaprojects/client/src/main/java/minigames/client/peggle/InGameUI.java
+++ javaprojects/client/src/main/java/minigames/client/peggle/InGameUI.java
@@ -1,31 +1,37 @@
 package minigames.client.peggle;
 
 import javax.swing.JPanel;
-import javax.swing.ImageIcon;
-import javax.swing.event.MouseInputAdapter;
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+//import javax.swing.event.MouseInputAdapter;
 
 public class InGameUI extends JPanel {
     final int columns = 1000;
     final int rows = 750;
-    private boolean grid[][] = new boolean[rows][columns];
+    final Color background = Color.BLACK;
+
+    // int cell_width = 1; // preferred -NOT CURRENTLY USED
+    // int cell_height = 1; // preferred -NOT CURRENTLY USED
+
+    private final boolean[][] grid = new boolean[rows][columns];
     int hoverX = -1;
     int hoverY = -1;
-    //InGameUI Background image
-    private static final String backgroundImagePath = "./javaprojects/client/src/main/java/minigames/client/peggle/assets/UI/gameBG.png";
-    private ImageIcon backgroundImage = new ImageIcon(backgroundImagePath);
+
     //Instance of the Cannon class
-    private Cannon cannon;
+    private final Cannon cannon;
 
     InGameUI() {
-        // Creates the cannon at position (250, 55)
-        cannon = new Cannon(250, 55);
+        setBackground(background);
+        // Creates the cannon at position (0, 0)
+        cannon = new Cannon(0, 0);
 
+        /* NOT CURRENTLY USED
         // A new MouseInputAdapter can replace default mouse actions
         addMouseListener(new MouseInputAdapter() {
+
             // There are other methods too, like click (press and release in the same spot)
             // Add mousePressed and mouseReleased methods for dragging
             @Override
@@ -40,8 +46,10 @@ public class InGameUI extends JPanel {
                 // In some cases, we might want to react to an item being "dropped"
             }
         });
+        */
 
-        // A new MouseMotionListener can handle movements
+        // A MouseMotionListener can handle movements
+        // Used for cannon to track mouse location if mouse moved or dragged
         addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
@@ -53,10 +61,10 @@ public class InGameUI extends JPanel {
                 handleHover(e);
             }
         });
-
     }
 
     void handleHover(MouseEvent e) {
+        /* NOT CURRENTLY USED
         int x = columns * e.getX() / getWidth();
         int y = (rows * e.getY() / getHeight());
         if (x == hoverX && y == hoverY) {
@@ -66,15 +74,12 @@ public class InGameUI extends JPanel {
         hoverY = (rows - 1) - y; // Fix for upside down coordinates
         hoverX = x;
         repaint();
+         */
 
-        // Ignore mouse movements above a certain Y position
-        if (e.getY() < 80) {
-            return;
-        }
+        //Update the angle of the cannon to point towards the mouse position
+        int dx = e.getX() - cannon.x;
+        int dy = e.getY() - cannon.y;
 
-        // Update the angle of the cannon to point towards the mouse position
-        double dx = e.getX() - cannon.x;
-        double dy = e.getY() - cannon.y;
         double angle = Math.atan2(dy, dx);
         cannon.setAngle(angle);
         repaint();
@@ -82,21 +87,29 @@ public class InGameUI extends JPanel {
 
     // Handles clicks or drags
     void handleDrag(MouseEvent e) {
+        /* NOT CURRENTLY USED
         // Be careful, it will keep registering dragging as we go off the window
         if (e.getX() < 0 || e.getX() > getWidth() || e.getY() < 0 || e.getY() > getHeight()) {
             return;
         }
-        // Track and paint the yellow cursor bit while we're dragging
+        */
+
+        // Tracks cannon while mouse key pressed+dragged
         handleHover(e);
 
-        int x = columns * e.getX() / getWidth();
-        int y = (rows * e.getY() / getHeight());
-        y = (rows - 1) - y; // Fix for upside down coordinates
+        /* NOT CURRENTLY USED
+        //int x = columns * e.getX() / getWidth();
+        //int y = (rows * e.getY() / getHeight());
+        //y = (rows - 1) - y; // Fix for upside down coordinates
+
         // Update the model
-        grid[y][x] = true;
+        //grid[y][x] = true;
+        */
+
         repaint();
     }
 
+    /* NOT CURRENTLY USED
     private void drawGridSquare(int x, int y, Graphics g) {
         // Translate x and y to panel coordinates
         // Switch y coordinates that usually start from top, to start from bottom.
@@ -104,6 +117,7 @@ public class InGameUI extends JPanel {
         y = (int) (getHeight() * (rows - 1 - y) / rows);
         g.fillRect(x, y, getWidth() / columns, getHeight() / rows);
     }
+     */
 
     // This method can be used to paint a Component however you like
     // It will be called whenever you repaint(), or when the screen is resized etc
@@ -111,28 +125,10 @@ public class InGameUI extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Draw the background image
-        g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), this);
-
-        Graphics2D g2d = (Graphics2D) g;
-
-        // Update the cannon's position to be the center of the window
+        // Centers the cannon, even when window is resized
         cannon.setX(getWidth() / 2);
 
-        for (int y = 0; y < rows; y++) {
-            for (int x = 0; x < columns; x++) {
-                if (!grid[y][x])
-                    continue;
-                // Grey squares
-                drawGridSquare(x, y, g);
-            }
-        }
-
-        drawGridSquare(hoverX, hoverY, g);
-
         // Draw the cannon
-        cannon.draw(g2d);
+        cannon.draw(g);
     }
-
-
 }
