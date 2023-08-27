f83b60796f85d4e87131a07635ae1ef3f27cb3f3
Anonymized Author <anon@example.com>
2023-08-27 16:17:16 +1000

    Memory game - create new branch to restructure GUI

--- javaprojects/client/src/main/java/minigames/client/memory/Memory.java
+++ javaprojects/client/src/main/java/minigames/client/memory/Memory.java
@@ -23,7 +23,7 @@ import javax.swing.Timer;
  * @author Melinda Luo, Lyam Talbot, Scott Lehmann, William Koller
  * Memory Card Game
  */
-public class Memory implements GameClient {
+public class Memory implements GameClient{ //, ActionListener, MouseListener {
 
     // Client instance
     MinigameNetworkClient mnClient;
@@ -35,29 +35,30 @@ public class Memory implements GameClient {
     String player;
 
     /** Swing components */
-    JPanel mainPanel; // Main panel for the game
-    JPanel gameMenuPanel; // Panel for the game menu which houses the options for the game
-    JPanel commandPanel; // Panel for the game to react to user input such as "it's a pair"
-    JPanel headingPanel; // Panel that houses the game and player objects
-    JLabel title;
-    JPanel playerPanel; // Panel for the player information
-    JLabel playerName;
-    JLabel matches;
-    JLabel stopwatch;
-    JPanel gameOptionsPanel; // Houses the button objects for the game options
-    JButton newGameButton;
-    JButton restartLevelButton;
-    JButton exitButton;
-    JButton achievementsButton;
-    JPanel gridContainerPanel; // Wraps the cardGridPanel
-    JPanel cardGridPanel; // Sets the grid for the playing cards
 
-    ImageIcon cardBackImage = new ImageIcon(getClass().getResource("/memory/images/playing_cards/back/card_back_black.png"));
+    //JPanel mainPanel; // Main panel for the game
+    //JPanel gameMenuPanel; // Panel for the game menu which houses the options for the game
+    //JPanel commandPanel; // Panel for the game to react to user input such as "it's a pair"
+    //JPanel headingPanel; // Panel that houses the game and player objects
+    //JLabel title;
+    //JPanel playerPanel; // Panel for the player information
+    JLabel playerName;
+    //JLabel matches;
+    //JLabel stopwatch;
+    //JPanel gameOptionsPanel; // Houses the button objects for the game options
+    //JButton newGameButton;
+    //JButton restartLevelButton;
+    //JButton exitButton;
+    //JButton achievementsButton;
+    //JPanel gridContainerPanel; // Wraps the cardGridPanel
+    //JPanel cardGridPanel; // Sets the grid for the playing cards
+
+    //ImageIcon cardBackImage = new ImageIcon(getClass().getResource("/memory/images/playing_cards/back/card_back_black.png"));
     //ImageIcon clubs_2 = new ImageIcon(getClass().getResource("/memory/images/playing_cards/2_of_clubs.png"));
 
     // Path to card images directory (card front images only)
-    String cardImagesDirectory = getClass().getResource("/memory/images/playing_cards/front/").getPath();
-
+    //String cardImagesDirectory = getClass().getResource("/memory/images/playing_cards/front/").getPath();
+        
     JTextArea textArea;
 
     // Game variable
@@ -68,79 +69,34 @@ public class Memory implements GameClient {
     int rows = 4; // Default rows
     int columns = 4; // Default columns
 
+    MemoryGame game;
+    MemoryGUI GUI;
+    JFrame MemoryWindow;
+
     /** Initialize Swing components */
     public Memory() {
-        headingPanel = new JPanel();
-        headingPanel.setLayout(new GridLayout(1, 1));
-        title = new JLabel("Pair Up - A Memory Card Game");
-        title.setFont(new Font("Comic Sans MS", Font.BOLD, 25));
-        title.setHorizontalAlignment(JLabel.CENTER);
-        headingPanel.add(title);
-
-        // Create the game menu panel
-        gameMenuPanel = new JPanel();
-        gameMenuPanel.setLayout(new GridLayout(3, 0));
-        gameMenuPanel.setBorder((new EmptyBorder(0, 0, 15, 0)));
-        gameMenuPanel.add(headingPanel); // Add heading panel to the game menu panel
-
-        // Create the player panel
-        playerPanel = new JPanel();
-        playerPanel.setLayout(new GridLayout(1, 2));
-        playerPanel.setPreferredSize(new Dimension(800, 30));
-
-        playerName = new JLabel("Player: " + player); // Placeholder text for player name
-        playerName.setFont(new Font("Arial", Font.BOLD, 16));
-        playerName.setHorizontalAlignment(JLabel.LEFT);
-
-        matches = new JLabel("Pairs matched: " + matchesCounter + "/8"); // Placeholder text for matched pairs
-        matches.setFont(new Font("Arial", Font.BOLD, 16));
-        matches.setHorizontalAlignment(JLabel.CENTER);
-
-        stopwatch = new JLabel(String.format("Time elapsed: %02d:%02d", timeElapsed[0], timeElapsed[1])); // Placeholder text for Timer
-        stopwatch.setFont(new Font("Arial", Font.BOLD, 16));
-        stopwatch.setHorizontalAlignment(JLabel.RIGHT);
-
-        // Add the player information to the player panel
-        playerPanel.add(playerName);
-        playerPanel.add(matches);
-        playerPanel.add(stopwatch);
+        game = new MemoryGame();
+        GUI = new MemoryGUI();
+        MemoryWindow = new JFrame("Memory");
+        MemoryWindow.setSize(800,800);
+        MemoryWindow.add(this.GUI);
 
 
-        // Create the game options panel which hosts new game, restart level, and exit buttons
-        gameOptionsPanel = new JPanel();
-        gameOptionsPanel.setLayout(new GridLayout(1, 5));
-
-        newGameButton = new JButton("New Game");
-        newGameButton.setFont(new Font("Arial", Font.BOLD, 16));
-
-        restartLevelButton = new JButton("Restart Level");
-        restartLevelButton.setFont(new Font("Arial", Font.BOLD, 16));
-
-        exitButton = new JButton("Exit");
-        exitButton.setFont(new Font("Arial", Font.BOLD, 16));
-        exitButton.addActionListener(e -> mnClient.runMainMenuSequence());
-
-        // Create a JComboBox to select difficulty level. This updates the cardGridPanel.
-        JComboBox<String> difficultyComboBox = new JComboBox<>(new String[]{"Easy", "Medium", "Hard"});
-        difficultyComboBox.setFont(new Font("Arial", Font.BOLD, 16));
-        difficultyComboBox.setSelectedItem("Medium");
-
-        // Create achievements button that talks to the API
-        achievementsButton = new JButton("Achievements");
-        achievementsButton.setFont(new Font("Arial", Font.BOLD, 16));
-        //achievementsButton.addActionListener(e -> mnClient.getGameAchievements(player, gm.gameServer()));
+    }
 
 
-        // Add game options to gameOptionsPanel
-        gameOptionsPanel.add(difficultyComboBox);
-        gameOptionsPanel.add(achievementsButton);
-        gameOptionsPanel.add(newGameButton);
-        gameOptionsPanel.add(restartLevelButton);
-        gameOptionsPanel.add(exitButton);
 
-        // Add playerPanel and gameOptionsPanel to gameMenuPanel
-        gameMenuPanel.add(playerPanel);
-        gameMenuPanel.add(gameOptionsPanel);
+
+
+
+
+
+
+
+/*
+    public void MemoryGUI() {   
+
+
 
         // Create a list of card images
         List<ImageIcon> cardImages = new ArrayList<>();
@@ -191,7 +147,7 @@ public class Memory implements GameClient {
             cardImages.remove(randomIndex); // Then remove selected card
         } */
 
-
+/*
         // Shuffle the card pairs - FIXME: Use the shuffling framework/system here!
         Collections.shuffle(cardPairs);
 
@@ -293,19 +249,33 @@ public class Memory implements GameClient {
 
         // Set up the main layout
         mainPanel = new JPanel();
+        //mainPanel = new JFrame();
+        //mainPanel.setSize(800,800);
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(gameMenuPanel, BorderLayout.NORTH);
         mainPanel.add(gridContainerPanel, BorderLayout.CENTER);
         //mainPanel.add(commandPanel, BorderLayout.SOUTH);
 
 
-        for (Component c : new Component[]{title, headingPanel, playerName, newGameButton, restartLevelButton, exitButton,
-                cardGridPanel, commandPanel, textArea, gameMenuPanel, playerPanel, gridContainerPanel, mainPanel})
-            c.setBackground(Color.decode("#eeeeee"));
-    }
+        //for (Component c:  new Component[] {title, headingPanel, playerName, newGameButton, restartLevelButton, exitButton,
+                //cardGridPanel, commandPanel, textArea, gameMenuPanel, playerPanel, gridContainerPanel, mainPanel})
+            //c.setBackground(Color.decode("#B2C6B2"));
+
+        // Add ActionListeners to the buttons
+        newGameButton.addActionListener(this);
+        restartLevelButton.addActionListener(this);
+        exitButton.addActionListener(this);
+
+        // Add ActionListeners to the card grid
+        for (Component c : cardGridPanel.getComponents()) {
+            c.addMouseListener(this);
+        }
+    } */
 
     // FIX - How to revert back to original (e.g. 1 minute) once Game Over message pops up and player clicks "OK"?
     // TimerListener implementing ActionListener - Define timer countdown & call method to update stopwatch
+    
+    /* 
     public class TimerListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -325,11 +295,11 @@ public class Memory implements GameClient {
             updateStopwatch();
         }
     }
-
+*/
     // Method to update stopwatch
-    private void updateStopwatch() {
+    /*private void updateStopwatch() {
         stopwatch.setText(String.format("Time elapsed: %02d:%02d", timeElapsed[0], timeElapsed[1]));
-    }
+    }*/
 
     // Run
     public static void main(String[] args) {
@@ -361,22 +331,23 @@ public class Memory implements GameClient {
         this.mnClient = mnClient;
         this.gm = game;
         this.player = player;
-
-        playerName.setText("Player: " + player);
+        //playerName.setText("Player: " + player);
 
         // Add our components to the north, south, east, west, or centre of the main window's BorderLayout
-        mnClient.getMainWindow().addCenter(mainPanel);  
-        
+        //mnClient.getMainWindow().addCenter(mainPanel);
+        mnClient.getMainWindow().addCenter(GUI);
         // FIX - Ask player which level difficulty they want to try before this pops up?
         // Set popup message to ask if player wants to start game: YES or NO
+        
+        /* 
         int choice = JOptionPane.showConfirmDialog(null, "Start the game?", "Memory Card Game", JOptionPane.YES_NO_OPTION);
         if (choice == JOptionPane.YES_OPTION) {
             // On selection of YES - Timer for stopwatch starts countdown
             timer = new Timer(1000, new TimerListener());
             timer.start();
         }
-
-        textArea.append("Starting...");
+*/
+        //textArea.append("Starting...");
 
         // Don't forget to call pack - it triggers the window to resize and repaint itself
         mnClient.getMainWindow().pack();
@@ -390,11 +361,90 @@ public class Memory implements GameClient {
         // Note that this uses the -> version of case statements, not the : version
         // (which means we don't need to say "break;" at the end of our cases)
         switch (command.getString("command")) {
-            case "clearText" -> textArea.setText("");
-            case "appendText" -> textArea.setText(textArea.getText() + command.getString("text"));
+           // case "clearText" -> textArea.setText("");
+           // case "appendText" -> textArea.setText(textArea.getText() + command.getString("text"));
         }
     }
+ 
 
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+
+/*
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == newGameButton) {
+            //TODO
+            mnClient.send(new CommandPackage(gm.gameServer(), gm.name(), player, Collections.singletonList(new JsonObject().put("command", "newGame"))));
+            System.out.println("New game started");
+        }
+        if (e.getSource() == restartLevelButton) {
+            //TODO
+           mnClient.send(new CommandPackage(gm.gameServer(), gm.name(), player, Collections.singletonList(new JsonObject().put("command", "restartLevel"))));
+            System.out.println("Level Restarted...");
+        }
+        if (e.getSource() == exitButton) {
+            //TODO
+            closeGame();
+            System.out.println("Testing...");
+        }
+
+    }
+*/
+    /**
+     * Invoked when the mouse button has been clicked (pressed
+     * and released) on a component.
+     *
+     * @param e the event to be processed
+     */
+    //@Override
+    //public void mouseClicked(MouseEvent e) {
+
+    //}
+
+    /**
+     * Invoked when a mouse button has been pressed on a component.
+     *
+     * @param e the event to be processed
+     */
+    //@Override
+    //public void mousePressed(MouseEvent e) {
+
+    //}
+
+    /**
+     * Invoked when a mouse button has been released on a component.
+     *
+     * @param e the event to be processed
+     */
+    //@Override
+    //public void mouseReleased(MouseEvent e) {
+
+    //}
+
+    /**
+     * Invoked when the mouse enters a component.
+     *
+     * @param e the event to be processed
+     */
+    //@Override
+    //public void mouseEntered(MouseEvent e) {
+
+    //}
+
+    /**
+     * Invoked when the mouse exits a component.
+     *
+     * @param e the event to be processed
+     */
+    //@Override
+    //public void mouseExited(MouseEvent e) {
+
+    //}
     /**
      * a method to resize the ImageIcon's image and return the resized ImageIcon
      * @param originalImageIcon the original ImageIcon to be resized
@@ -404,10 +454,10 @@ public class Memory implements GameClient {
      *      *             substituted to maintain the aspect ratio of the original image dimensions)
      * @return resized ImageIcon
      */
-    public ImageIcon resizeImageIcon(ImageIcon originalImageIcon, int targetWidth, int targetHeight){
-    Image resizedImage = originalImageIcon.getImage().getScaledInstance(targetWidth,targetHeight,Image.SCALE_SMOOTH);
-    return new ImageIcon(resizedImage);
-    }
+    //public ImageIcon resizeImageIcon(ImageIcon originalImageIcon, int targetWidth, int targetHeight){
+    //Image resizedImage = originalImageIcon.getImage().getScaledInstance(targetWidth,targetHeight,Image.SCALE_SMOOTH);
+    //return new ImageIcon(resizedImage);
+    //}
 
     @Override
     public void closeGame() {
---
+++ javaprojects/client/src/main/java/minigames/client/memory/MemoryGame.java
@@ -0,0 +1,15 @@
+package minigames.client.memory;
+
+
+
+
+
+public class MemoryGame {
+
+    //public DeckOfCards.PlayingCard [] flippedCards = {null, null};
+    //private DeckOfCards deckOfCards = new DeckOfCards(16, true);
+
+
+}
+
+
