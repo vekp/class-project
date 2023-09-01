36eb26034d58d42386e62dd4c07ded7b2048af6f
Anonymized Author <anon@example.com>
2023-09-01 21:31:42 +1000

    Updated LevelUp.java

--- javaprojects/client/src/main/java/minigames/client/krumgame/LevelUp.java
+++ javaprojects/client/src/main/java/minigames/client/krumgame/LevelUp.java
@@ -1,52 +1,54 @@
 package minigames.client.krumgame;
 
+/**
+ * This class will be used in the Game class 
+ * for username, xp and level. This class communicates 
+ * with the MYSQLconnector class to connect with the 
+ * database and retrieve or update the xp. 
+ */
+
 public class LevelUp {
 
-    public static int xp;
-    public static int level;
-
-    //TO DO - Make global variable of Username, crosschecked with the MySQL database also 
-    // and at the same time, saving the XP to a local variable 'xp' 
-
-    //TO DO - Write reverse code that adds more XP to MySQL database at the end of every game when xp
-    // has been earned. If playing two players, this will have to be done for each username.
-
-
-    // The below function will call the MySQLConnector class to connect to the database and source
-    // the appropriate data (xp) from the MySQL database
-    
-
-    public static int loadXp() {
-        //MySQLConnector class
-        //read file info for xp from database
-        //below is a temp test value
-        xp = 10;
-        return xp;
-    }
-
-    //This function figures out what level a character is
-
-    public static int calculateLevel() {
-        //Calculate level based on total XP
-        if (xp > 500) {
-            level = 5;
-        }else {
-            level = xp/100;
-        }
-        return level;
-    }
-
-
-    public static void main(String[] args) {
-        
-        //read username file info from database
+    private int xp;
+    private int level;
+    private String username; 
+    /**
+     * Constructor to initialize the username, xp and level.
+     */
+    public LevelUp(String username) {
+        this.username = username;
         loadXp();
         calculateLevel();
+    }
+    /**
+     * This method will load xp.
+     */
+    private void loadXp() {
+        this.xp = MySQLConnector.getXpForUser(username);
+    }
 
-        System.out.println("Your XP is currently: " + xp);
-        System.out.println("Your level is currently: " + level);
+    /**
+     * This method will update xp for the player
+     */
+    public void updateXp(int newXp) {
+        MySQLConnector.updateXpForUser(username, newXp);
+        calculateLevel();
+    }
 
+    private int calculateLevel() {
+        if (xp > 500) {
+            this.level = 5;
+        } else {
+            this.level = xp / 100;
+        }
+        return this.level;
+    }
 
-       //Must also return the Level and XP for use in the game
+    public int getXp() {
+        return this.xp;
+    }
+
+    public int getLevel() {
+        return this.level;
     }
 }
