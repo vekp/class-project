8590f7961e6b53c203c3152f114ad98a50c983a9
Anonymized Author <anon@example.com>
2023-09-02 23:56:55 +1000

    updated back button for score and help panel and remove unwanted files

--- javaprojects/server/src/main/java/minigames/server/snakeGameServer/Main.java
+++
@@ -1,9 +0,0 @@
-// Main class to start the game
-public class Main {
-
-    public static void main(String[] args) {
-        // Create a new instance of the GameFrame class to start the game
-        new GameFrame();
-    }
-}
-
