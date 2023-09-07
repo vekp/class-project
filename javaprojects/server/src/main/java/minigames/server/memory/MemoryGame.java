44276ce10372a50ff6c33588cce0e080bd239a93
Anonymized Author <anon@example.com>
2023-09-07 15:32:26 +1000

    Make basic method for checking card values against each other. Method updates score.

--- javaprojects/server/src/main/java/minigames/server/memory/MemoryGame.java
+++ javaprojects/server/src/main/java/minigames/server/memory/MemoryGame.java
@@ -16,6 +16,7 @@ import org.apache.logging.log4j.Logger;
 
 import minigames.server.achievements.AchievementHandler;
 import static minigames.server.memory.MemoryAchievement.*;
+
 /**
  * Represents an actual Memory game in progress.
  * Used and adapted MuddleGame.java
@@ -26,7 +27,8 @@ public class MemoryGame {
     private static final Logger logger = LogManager.getLogger(MemoryGame.class);
 
     // Create a record of MemoryPlayer class
-    record MemoryPlayer(String name) {}
+    record MemoryPlayer(String name) {
+    }
 
     /** Uniquely identifies this game */
     String name;
@@ -34,6 +36,9 @@ public class MemoryGame {
     AchievementHandler achievementHandler;
     String cardToShow = "_2_of_Clubs.png";
     PlayingCard[] playingCards;
+    // PlayingCard previousCardIndex = null;
+    int previousCardIndex = -1;
+    int playerScore = 0;
 
     public MemoryGame(String name, String playerName) {
         this.name = name;
@@ -54,6 +59,25 @@ public class MemoryGame {
     // Players
     HashMap<String, MemoryPlayer> players = new HashMap<>();
 
+    public void check(int cardIndex) {
+        if (previousCardIndex != -1) {
+            if (playingCards[previousCardIndex].equals(playingCards[cardIndex])) {
+                playerScore++;
+                System.out.println("These cards match!");
+                System.out.println(playerScore);
+                previousCardIndex = -1;
+                return;
+            } else {
+                previousCardIndex = -1;
+                System.out.println("These cards do not match");
+                return;
+            }
+        } else {
+            previousCardIndex = cardIndex;
+            return;
+        }
+    }
+
     /** The players currently playing this game */
     public String[] getPlayerNames() {
         return players.keySet().toArray(String[]::new);
@@ -66,7 +90,7 @@ public class MemoryGame {
         return new GameMetadata("Memory", name, getPlayerNames(), true);
     }
 
-    public RenderingPackage runCommands(CommandPackage cp) {   
+    public RenderingPackage runCommands(CommandPackage cp) {
         logger.info("Received command package {}", cp);
 
         // Create temp JSONObject object
@@ -80,77 +104,93 @@ public class MemoryGame {
             case "Flip_Card_1" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_1"));
                 System.out.println(playingCards[0].getValue() + " of " + playingCards[0].getSuit());
+                check(0);
             }
             case "Flip_Card_2" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_2"));
                 System.out.println(playingCards[1].getValue() + " of " + playingCards[1].getSuit());
-
+                check(1);            
             }
             case "Flip_Card_3" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_3"));
                 System.out.println(playingCards[2].getValue() + " of " + playingCards[2].getSuit());
+                check(2);
             }
             case "Flip_Card_4" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_4"));
                 System.out.println(playingCards[3].getValue() + " of " + playingCards[3].getSuit());
+                check(3);
             }
             case "Flip_Card_5" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_5"));
                 System.out.println(playingCards[4].getValue() + " of " + playingCards[4].getSuit());
+                check(4);
             }
             case "Flip_Card_6" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_6"));
                 System.out.println(playingCards[5].getValue() + " of " + playingCards[5].getSuit());
+                check(5);
             }
             case "Flip_Card_7" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_7"));
                 System.out.println(playingCards[6].getValue() + " of " + playingCards[6].getSuit());
+                check(6);
             }
             case "Flip_Card_8" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_8"));
                 System.out.println(playingCards[7].getValue() + " of " + playingCards[7].getSuit());
+                check(7);
             }
             case "Flip_Card_9" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_9"));
                 System.out.println(playingCards[8].getValue() + " of " + playingCards[8].getSuit());
+                check(8);
             }
             case "Flip_Card_10" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_10"));
                 System.out.println(playingCards[9].getValue() + " of " + playingCards[9].getSuit());
+                check(9);
             }
             case "Flip_Card_11" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_11"));
                 System.out.println(playingCards[10].getValue() + " of " + playingCards[10].getSuit());
+                check(10);
             }
             case "Flip_Card_12" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_12"));
                 System.out.println(playingCards[11].getValue() + " of " + playingCards[11].getSuit());
+                check(11);
             }
             case "Flip_Card_13" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_13"));
                 System.out.println(playingCards[12].getValue() + " of " + playingCards[12].getSuit());
+                check(12);
             }
             case "Flip_Card_14" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_14"));
                 System.out.println(playingCards[13].getValue() + " of " + playingCards[13].getSuit());
+                check(13);
             }
             case "Flip_Card_15" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_15"));
                 System.out.println(playingCards[14].getValue() + " of " + playingCards[14].getSuit());
+                check(14);
             }
             case "Flip_Card_16" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_16"));
                 System.out.println(playingCards[15].getValue() + " of " + playingCards[15].getSuit());
+                check(15);
             }
             case "Flip_Card_17" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_17"));
                 System.out.println(playingCards[16].getValue() + " of " + playingCards[16].getSuit());
+                check(16);
             }
             case "Flip_Card_18" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_18"));
                 System.out.println(playingCards[17].getValue() + " of " + playingCards[17].getSuit());
+                check(17);
             }
-
 
         }
 
@@ -161,11 +201,10 @@ public class MemoryGame {
     public RenderingPackage joinGame(String playerName) {
         if (players.containsKey(playerName)) {
             return new RenderingPackage(
-                gameMetadata(),
-                Arrays.stream(new RenderingCommand[] {
-                    new NativeCommands.ShowMenuError("That name's not available")
-                }).map((r) -> r.toJson()).toList()
-            );
+                    gameMetadata(),
+                    Arrays.stream(new RenderingCommand[] {
+                            new NativeCommands.ShowMenuError("That name's not available")
+                    }).map((r) -> r.toJson()).toList());
         } else {
             MemoryPlayer p = new MemoryPlayer(playerName);
             players.put(playerName, p);
