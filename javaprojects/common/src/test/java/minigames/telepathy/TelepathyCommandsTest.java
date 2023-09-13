4a0bbefd88c2c941ccfd7215903c31b7b38f9d1b
Anonymized Author <anon@example.com>
2023-09-13 10:15:02 +0800

    Update TelepathyCommands with current values

--- javaprojects/common/src/test/java/minigames/telepathy/TelepathyCommandsTest.java
+++ javaprojects/common/src/test/java/minigames/telepathy/TelepathyCommandsTest.java
@@ -13,7 +13,6 @@ public class TelepathyCommandsTest {
         assertTrue(TelepathyCommands.MODIFYPLAYER.toString().equals("MODIFYPLAYER"));
         assertTrue(TelepathyCommands.QUIT.toString().equals("QUIT"));
         assertTrue(TelepathyCommands.SYSTEMQUIT.toString().equals("SYSTEMQUIT"));
-        assertTrue(TelepathyCommands.BUTTONPRESS.toString().equals("BUTTONPRESS"));
         assertTrue(TelepathyCommands.BUTTONUPDATE.toString().equals("BUTTONUPDATE"));
         assertTrue(TelepathyCommands.REQUESTUPDATE.toString().equals("REQUESTUPDATE"));
         assertTrue(TelepathyCommands.NOUPDATE.toString().equals("NOUPDATE"));
@@ -21,7 +20,14 @@ public class TelepathyCommandsTest {
         assertTrue(TelepathyCommands.JOINGAMEFAIL.toString().equals("JOINGAMEFAIL"));
         assertTrue(TelepathyCommands.JOINGAMESUCCESS.toString().equals("JOINGAMESUCCESS"));
         assertTrue(TelepathyCommands.GAMEOVER.toString().equals("GAMEOVER"));
+        assertTrue(TelepathyCommands.CHOOSETILE.toString().equals("CHOOSETILE"));
+        assertTrue(TelepathyCommands.ASKQUESTION.toString().equals("ASKQUESTION"));
+        assertTrue(TelepathyCommands.FINALGUESS.toString().equals("FINALGUESS"));
+        assertTrue(TelepathyCommands.POPUP.toString().equals("POPUP"));
 
         assertTrue(TelepathyCommands.TESTCOMMAND.toString().equals("TESTCOMMAND"));
+
+        // The default case needs to create an error string
+        assertTrue(TelepathyCommands.DEFAULT.toString().equals("NO-STRING-CONFIGURED"));
     }
 }
