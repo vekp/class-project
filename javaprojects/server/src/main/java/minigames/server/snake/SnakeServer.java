2bc05f8b850d860973539284ecf9837a5ddade93
Anonymized Author <anon@example.com>
2023-09-18 06:04:56 +1000

    Updated game description

--- javaprojects/server/src/main/java/minigames/server/snake/SnakeServer.java
+++ javaprojects/server/src/main/java/minigames/server/snake/SnakeServer.java
@@ -55,7 +55,7 @@ public class SnakeServer implements GameServer {
      * @return GameServerDetails object with the name and description of the game.
      */
     public GameServerDetails getDetails() {
-        return new GameServerDetails("Snake", "Navigate your snake through the dark depths of space!");
+        return new GameServerDetails("Snake", "A trip down memory lane");
     }
 
     @Override
