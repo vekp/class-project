4a229de7b851268010e79f49867f073c1389c7d1
Anonymized Author <anon@example.com>
2023-09-19 03:26:10 +1000

    Added BackgroundContainer class for Snake game UI
    This class provides a background with a centered game logo for the Snake game UI, enhancing its visual appeal.

---
+++ javaprojects/client/src/main/java/minigames/client/snake/BackgroundContainer.java
@@ -0,0 +1,40 @@
+package minigames.client.snake;
+
+import javax.swing.*;
+
+/**
+ * The BackgroundContainer class provides a standardized background with a logo for the Snake game UI.
+ */
+public class BackgroundContainer extends JLabel {
+
+    /**
+     * Constructs the BackgroundContainer and sets up the background with a logo.
+     */
+    public BackgroundContainer() {
+        setupBackgroundWithLogo();
+    }
+
+    /**
+     * Configures the background image and positions the logo centrally.
+     */
+    private void setupBackgroundWithLogo() {
+        // Setup the background image
+        ImageIcon backgroundIcon = MultimediaManager.getPhoneBackground().getImageResource();
+        this.setIcon(backgroundIcon);
+        this.setBounds(0, 0, backgroundIcon.getIconWidth(), backgroundIcon.getIconHeight());
+        this.setLayout(null);
+
+        // Create and position the logo label
+        ImageIcon logoIcon = MultimediaManager.getSnakeLogoResource().getImageResource();
+        int logoWidth = logoIcon.getIconWidth();
+        int logoHeight = logoIcon.getIconHeight();
+
+        // Calculate the position of the logo
+        int logoX = (this.getWidth() - logoWidth) / 2;
+        int logoY = GameConstants.LOGO_Y;
+
+        JLabel logoLabel = new JLabel(logoIcon);
+        logoLabel.setBounds(logoX, logoY, logoWidth, logoHeight);
+        this.add(logoLabel);
+    }
+}
