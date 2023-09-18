80e075190d56282abf916ae85b2bf34fa74f46f4
Anonymized Author <anon@example.com>
2023-09-19 03:34:03 +1000

    refactor: Enhance MainMenuPanel for improved structure and button setup
    This commit refactors the MainMenuPanel class to improve its structure and button setup. The changes include using the 'PanelSwitcher' interface for panel transitions, centralizing background label setup, and ensuring button consistency based on 'GameConstants'.

--- javaprojects/client/src/main/java/minigames/client/snake/MainMenuPanel.java
+++ javaprojects/client/src/main/java/minigames/client/snake/MainMenuPanel.java
@@ -1,58 +1,77 @@
 package minigames.client.snake;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
- * The MainMenuPanel class represents the main menu screen for the Snake game.
- * It is responsible for displaying the main menu background, initializing the multimedia manager
- * for sound playback, and providing methods to manage the background sound.
+ * MainMenuPanel represents the main menu of the Snake game.
+ * It provides buttons for starting the game, accessing help, about, achievements, and exiting the game.
  */
 public class MainMenuPanel extends JPanel {
-    // The multimedia manager to manage sounds and images for the game
-    private final MultimediaManager multimediaManager;
+    private final Runnable closeGameAction;
+    private final PanelSwitcher panelSwitcher;
+    private final JLabel backgroundLabel;
 
     /**
-     * Constructs the MainMenuPanel. Initializes the layout, size, background,
-     * and starts the background sound for the main menu.
+     * Interface for switching between panels in the main menu.
      */
-    public MainMenuPanel() {
-        this.multimediaManager = new MultimediaManager();
-
-        // Setup layout, size, and background
-        setLayout(null); // Using null layout to set absolute positions for components
-        // Set the preferred size based on the background image dimensions
-        setPreferredSize(new Dimension(MultimediaManager.getPhoneBackground().getWidth(), MultimediaManager.getPhoneBackground().getHeight()));
-        setupBackground();
-
-        // Play the background sound for the main menu
-        multimediaManager.playBackgroundSound("Menu");
+    public interface PanelSwitcher {
+        void switchToPanel(String panelName);
     }
 
     /**
-     * Sets up the background image for the main menu.
+     * Constructs a MainMenuPanel.
+     *
+     * @param closeGameAction The action to be performed when the game is closed.
+     * @param panelSwitcher   The panel switcher for transitioning between menu panels.
      */
-    private void setupBackground() {
-        ImageIcon backgroundIcon = MultimediaManager.getPhoneBackground().getImage();
+    public MainMenuPanel(Runnable closeGameAction, PanelSwitcher panelSwitcher) {
+        this.closeGameAction = closeGameAction;
+        this.panelSwitcher = panelSwitcher;
+
+        setLayout(null);
+        backgroundLabel = createBackgroundLabel();
+        setupButtons();
+        // Play menu music
+        MultimediaManager.playBackgroundSound(GameConstants.MENU_MUSIC);
+    }
+
+    /**
+     * Creates and configures the background label for the main menu.
+     *
+     * @return The background label.
+     */
+    private JLabel createBackgroundLabel() {
+        ImageIcon backgroundIcon = MultimediaManager.getPhoneBackground().getImageResource();
         JLabel backgroundLabel = new JLabel(backgroundIcon);
-        backgroundLabel.setBounds(0, 0, backgroundIcon.getIconWidth(), backgroundIcon.getIconHeight()); // Set the size of the label to the size of the image
-        add(backgroundLabel); // This should be the first component added to ensure it's rendered in the background
+        backgroundLabel.setBounds(0, 0, backgroundIcon.getIconWidth(), backgroundIcon.getIconHeight());
+        backgroundLabel.setLayout(null);
 
-        // Adding the Snake Logo to the top of the MainMenuPanel
-        ImageIcon logoIcon = MultimediaManager.getSnakeLogoResource().getImage();
+        ImageIcon logoIcon = MultimediaManager.getSnakeLogoResource().getImageResource();
+        int logoWidth = logoIcon.getIconWidth();
+        int logoHeight = logoIcon.getIconHeight();
+
+        int logoX = (backgroundLabel.getWidth() - logoWidth) / 2;
+        int logoY = GameConstants.LOGO_Y;
+
         JLabel logoLabel = new JLabel(logoIcon);
+        logoLabel.setBounds(logoX, logoY, logoWidth, logoHeight);
+        backgroundLabel.add(logoLabel);
 
-        // Calculate the x position to center the logo
-        int logoX = (backgroundIcon.getIconWidth() - logoIcon.getIconWidth()) / 2;
-        logoLabel.setBounds(logoX, 30, logoIcon.getIconWidth(), logoIcon.getIconHeight()); // 10 pixels from the top
+        add(backgroundLabel);
 
-        backgroundLabel.add(logoLabel); // Add the logo to the background label so it appears on top of the background
+        return backgroundLabel;
     }
 
     /**
-     * Stops the background sound that is currently playing.
+     * Sets up and adds buttons to the main menu panel.
      */
-    public void stopBackgroundSound() {
-        multimediaManager.stopBackgroundSound();
+    private void setupButtons() {
+        int buttonX = (MultimediaManager.getPhoneBackground().getImageResourceWidth() - GameConstants.BUTTON_WIDTH) / 2;
+        backgroundLabel.add(ButtonFactory.createButton(GameConstants.PLAY_PANEL, buttonX, GameConstants.START_BUTTON_Y, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, e -> panelSwitcher.switchToPanel(GameConstants.PLAY_PANEL)));
+        backgroundLabel.add(ButtonFactory.createButton(GameConstants.HELP_MENU_PANEL, buttonX, GameConstants.START_BUTTON_Y + GameConstants.BUTTON_HEIGHT + GameConstants.BUTTON_GAP, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, e -> panelSwitcher.switchToPanel(GameConstants.HELP_MENU_PANEL)));
+        backgroundLabel.add(ButtonFactory.createButton(GameConstants.ABOUT_ME_PANEL, buttonX, GameConstants.START_BUTTON_Y + 2 * GameConstants.BUTTON_HEIGHT + GameConstants.BUTTON_GAP * 2, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, e -> panelSwitcher.switchToPanel(GameConstants.ABOUT_ME_PANEL)));
+        backgroundLabel.add(ButtonFactory.createButton(GameConstants.ACHIEVEMENTS_PANEL, buttonX, GameConstants.START_BUTTON_Y + 3 * GameConstants.BUTTON_HEIGHT + GameConstants.BUTTON_GAP * 3, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, e -> panelSwitcher.switchToPanel(GameConstants.ACHIEVEMENTS_PANEL)));
+        backgroundLabel.add(ButtonFactory.createButton(GameConstants.EXIT_GAME, buttonX, GameConstants.START_BUTTON_Y + 4 * GameConstants.BUTTON_HEIGHT + GameConstants.BUTTON_GAP * 4, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, e -> closeGameAction.run()));
     }
+
 }
