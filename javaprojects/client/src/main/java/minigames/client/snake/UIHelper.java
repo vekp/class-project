d4007382617379d96a87c778d2a3f9addc14e595
Anonymized Author <anon@example.com>
2023-09-19 03:39:07 +1000

    Create UIHelper class for return button setup
    Added a new UIHelper class for setting up return buttons in Swing-based UI components. The class includes methods for creating buttons with custom positions and names, enhancing code organization and reusability.

---
+++ javaprojects/client/src/main/java/minigames/client/snake/UIHelper.java
@@ -0,0 +1,36 @@
+package minigames.client.snake;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class UIHelper {
+
+    /**
+     * Sets up a "Return to Main Menu" button with default position (centered) and name.
+     *
+     * @param panelSwitcher The interface used to switch between panels.
+     * @param container The container (like a JPanel) where the button will be added.
+     * @return JButton The created button.
+     */
+    public static JButton setupReturnButton(MainMenuPanel.PanelSwitcher panelSwitcher, JComponent container) {
+        int buttonX = (MultimediaManager.getPhoneBackground().getImageResourceWidth() - GameConstants.BUTTON_WIDTH) / 2;
+        return setupReturnButton(panelSwitcher, container, buttonX, GameConstants.RETURN_BUTTON_Y, GameConstants.MAIN_MENU_PANEL);
+    }
+
+    /**
+     * Sets up a button with custom position and name.
+     *
+     * @param panelSwitcher The interface used to switch between panels.
+     * @param container The container (like a JPanel) where the button will be added.
+     * @param x The x-coordinate of the button.
+     * @param y The y-coordinate of the button.
+     * @param buttonName The custom name for the button.
+     * @return JButton The created button.
+     */
+    public static JButton setupReturnButton(MainMenuPanel.PanelSwitcher panelSwitcher, JComponent container, int x, int y, String buttonName) {
+        ActionListener action = e -> panelSwitcher.switchToPanel(GameConstants.MAIN_MENU_PANEL);
+        JButton returnButton = ButtonFactory.createButton(buttonName, x, y, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, action);
+        container.add(returnButton);
+        return returnButton;
+    }
+}
