40a3e56dc686cf5ebf3ac172347a60f0f9089d5b
Anonymized Author <anon@example.com>
2023-09-18 23:10:46 +1000

    Updated Memory game (server-side) files due to merge conflicts
    * MemoryGame.java & MemoryServer.java updated following merge conflicts

--- javaprojects/server/src/main/java/minigames/server/memory/MemoryGame.java
+++ javaprojects/server/src/main/java/minigames/server/memory/MemoryGame.java
@@ -1,6 +1,5 @@
 package minigames.server.memory;
 
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import java.util.*;
 
@@ -46,16 +45,14 @@ public class MemoryGame {
         this.playerName = playerName;
         this.achievementHandler = new AchievementHandler(MemoryServer.class);
 
+        /** Achievement handler for this game */
         // Unlock TEST_THAT_MEMORY achievement for starting a new game
         achievementHandler.unlockAchievement(playerName, TEST_THAT_MEMORY.toString());
 
+        /** The deck of cards in use for the current game */
         this.playingCards = new DeckOfCards(18, true).getCards();
         ShufflingFramework.shuffle(playingCards);
     }
-
-    /** Achievement handler for this game */
-    // Code snippet from AchievementHandler.java
-    // private static final
 
     // Players
     HashMap<String, MemoryPlayer> players = new HashMap<>();
@@ -118,8 +115,6 @@ public class MemoryGame {
         return players.keySet().toArray(String[]::new);
     }
 
-    /** The deck of cards in use for the current game */
-
     /** Metadata for this game */
     public GameMetadata gameMetadata() {
         return new GameMetadata("Memory", name, getPlayerNames(), true);
@@ -136,9 +131,6 @@ public class MemoryGame {
                 players.remove(cp.player());
                 renderingCommands.add(new JsonObject().put("command", "showGames"));
             }
-            // case "updateCards" ->{
-            //     renderingCommands.add(new JsonObject().put("comand", "updateCards").put("deck", playingCards));
-            // }
             case "newGame" -> {
                 this.playingCards = new DeckOfCards(18, true).getCards();
                 ShufflingFramework.shuffle(playingCards);
@@ -165,13 +157,9 @@ public class MemoryGame {
                 result.put("command", "Flip_Card_1");
                 result.put("update", "true");
                 renderingCommands.add(result);
-
-
-                // System.out.println(playingCards[0].getValue() + " of " + playingCards[0].getSuit());
             }
             case "Flip_Card_2" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_2"));
-                // System.out.println(playingCards[1].getValue() + " of " + playingCards[1].getSuit());
                 if(check(1)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));            
                 }
@@ -183,7 +171,6 @@ public class MemoryGame {
             }
             case "Flip_Card_3" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_3"));
-                // System.out.println(playingCards[2].getValue() + " of " + playingCards[2].getSuit());
                 if(check(2)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -194,7 +181,6 @@ public class MemoryGame {
             }
             case "Flip_Card_4" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_4"));
-                // System.out.println(playingCards[3].getValue() + " of " + playingCards[3].getSuit());
                 if(check(3)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -205,7 +191,6 @@ public class MemoryGame {
             }
             case "Flip_Card_5" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_5"));
-                // System.out.println(playingCards[4].getValue() + " of " + playingCards[4].getSuit());
                 if(check(4)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -216,7 +201,6 @@ public class MemoryGame {
             }
             case "Flip_Card_6" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_6"));
-                // System.out.println(playingCards[5].getValue() + " of " + playingCards[5].getSuit());
                 if(check(5)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -227,7 +211,6 @@ public class MemoryGame {
             }
             case "Flip_Card_7" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_7"));
-                // System.out.println(playingCards[6].getValue() + " of " + playingCards[6].getSuit());
                 if(check(6)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -238,7 +221,6 @@ public class MemoryGame {
             }
             case "Flip_Card_8" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_8"));
-                // System.out.println(playingCards[7].getValue() + " of " + playingCards[7].getSuit());
                 if(check(7)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -249,7 +231,6 @@ public class MemoryGame {
             }
             case "Flip_Card_9" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_9"));
-                // System.out.println(playingCards[8].getValue() + " of " + playingCards[8].getSuit());
                 if(check(8)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -260,7 +241,6 @@ public class MemoryGame {
             }
             case "Flip_Card_10" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_10"));
-                // System.out.println(playingCards[9].getValue() + " of " + playingCards[9].getSuit());
                 if(check(9)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -271,7 +251,6 @@ public class MemoryGame {
             }
             case "Flip_Card_11" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_11"));
-                // System.out.println(playingCards[10].getValue() + " of " + playingCards[10].getSuit());
                 if(check(10)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -282,7 +261,6 @@ public class MemoryGame {
             }
             case "Flip_Card_12" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_12"));
-                // System.out.println(playingCards[11].getValue() + " of " + playingCards[11].getSuit());
                 if(check(11)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -293,7 +271,6 @@ public class MemoryGame {
             }
             case "Flip_Card_13" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_13"));
-                // System.out.println(playingCards[12].getValue() + " of " + playingCards[12].getSuit());
                 if(check(12)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -304,7 +281,6 @@ public class MemoryGame {
             }
             case "Flip_Card_14" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_14"));
-                // System.out.println(playingCards[13].getValue() + " of " + playingCards[13].getSuit());
                 if(check(13)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -315,7 +291,6 @@ public class MemoryGame {
             }
             case "Flip_Card_15" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_15"));
-                // System.out.println(playingCards[14].getValue() + " of " + playingCards[14].getSuit());
                 if(check(14)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -326,7 +301,6 @@ public class MemoryGame {
             }
             case "Flip_Card_16" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_16"));
-                // System.out.println(playingCards[15].getValue() + " of " + playingCards[15].getSuit());
                 if(check(15)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -337,7 +311,6 @@ public class MemoryGame {
             }
             case "Flip_Card_17" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_17"));
-                // System.out.println(playingCards[16].getValue() + " of " + playingCards[16].getSuit());
                 if(check(16)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -348,7 +321,6 @@ public class MemoryGame {
             }
             case "Flip_Card_18" -> {
                 renderingCommands.add(new JsonObject().put("command", "Flip_Card_18"));
-                // System.out.println(playingCards[17].getValue() + " of " + playingCards[17].getSuit());
                 if(check(17)){
                     renderingCommands.add(new JsonObject().put("command", "updateScore"));
                 }else {
@@ -378,7 +350,6 @@ public class MemoryGame {
             ArrayList<JsonObject> renderingCommands = new ArrayList<>();
             renderingCommands.add(new LoadClient("Memory", "Memory", name, playerName).toJson());
             renderingCommands.add(new JsonObject().put("command", "clearText"));
-            // renderingCommands.add(new JsonObject().put("command", "deckOfCards").put("cards", playingCards));
             renderingCommands.add(new JsonObject().put("command", "deckOfCards").put("cards", playingCards));
 
             return new RenderingPackage(gameMetadata(), renderingCommands);
