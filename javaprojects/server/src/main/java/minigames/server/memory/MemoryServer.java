cb780b8a65a63b3353acca730fc0e857e811706a
Anonymized Author <anon@example.com>
2023-09-18 20:44:51 +1000

    Cleaned up Memory game code & added comments
    * Cleaned up and update code for Memory game (client & server sides)
    * Added in comments for Memory game

--- javaprojects/server/src/main/java/minigames/server/memory/MemoryServer.java
+++ javaprojects/server/src/main/java/minigames/server/memory/MemoryServer.java
@@ -1,7 +1,6 @@
 package minigames.server.memory;
 
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
 
 import java.util.HashMap;
 import java.util.Random;
@@ -12,8 +11,6 @@ import minigames.rendering.GameServerDetails;
 import minigames.rendering.RenderingPackage;
 import minigames.server.ClientType;
 import minigames.server.GameServer;
-
-import minigames.achievements.Achievement;
 import minigames.server.achievements.AchievementHandler;
 
 /**
@@ -21,40 +18,10 @@ import minigames.server.achievements.AchievementHandler;
  * When it receives a CommandPackage, it finds the MemoryGame and calls it.
  * Used and adapted MuddleServer.java
  */
-
-/*enum achievements {
-    PERFECT_MATCH, SPEED_RUNNER, ACE_MATCHER, MEMORY_MASTER;
-    @Override
-    public String toString(){
-        switch(this) {
-            case PERFECT_MATCH:         return "Perfect Match";
-            case SPEED_RUNNER:          return "Speed Runner";
-            case ACE_MATCHER:           return "Ace Matcher";
-            case MEMORY_MASTER:         return "Memory Master";
-            default:                    return "Unknown Achievement";
-        }
-    }
-}*/
-
 public class MemoryServer implements GameServer {
+    
     static final String chars = "abcdefghijklmopqrstuvwxyz";
     AchievementHandler achievementHandler;
-
-    /*
-    public MemoryServer() {
-        achievementHandler = new AchievementHandler(MemoryServer.class);
-        /* Create the achievements and give them to the handler
-        achievementHandler.registerAchievement(new Achievement(achievements.PERFECT_MATCH.toString(),
-                "Earned by matching all of the cards in one turn.", 25, "", false));
-        achievementHandler.registerAchievement(new Achievement(achievements.SPEED_RUNNER.toString(),
-                "Earned by matching all of the cards in under a certain amount of time.", 25, "", false));
-        achievementHandler.registerAchievement(new Achievement(achievements.ACE_MATCHER.toString(),
-                "Earned by matching all of the cards in a single game.", 25, "", false));
-        achievementHandler.registerAchievement(new Achievement(achievements.MEMORY_MASTER.toString(),
-                "Earned by earning all of the achievements in the game.", 25, "confused", false));
-    }
-    */
-
 
     /** A random name. We could do with something more memorable, like Docker has. */
     static String randomName() {
@@ -113,25 +80,4 @@ public class MemoryServer implements GameServer {
         return Future.succeededFuture(g.runCommands(cp));
     }
 
-    /* 
-        @Override
-        public Future<RenderingPackage> callGame(CommandPackage cp) {
-            MemoryGame g = games.get(cp.gameId());
-
-            // Create temp JSONObject object
-            JsonObject cmd = cp.commands().get(0);
-            switch (cmd.getString("command")) {
-                case "clearServer" -> {
-                    clearServer(cp.gameId());
-                }
-            }
-            return Future.succeededFuture(g.runCommands(cp));
-        }
-
-        private void clearServer(String s) {
-            games.remove(s);
-        }
-    
-     */
-    
 }
