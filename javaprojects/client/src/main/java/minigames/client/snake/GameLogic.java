64bb981c7f05094fcf43e59fb26bc88b5a15e04b
Anonymized Author <anon@example.com>
2023-09-19 03:30:57 +1000

    Add initial GameLogic class
    Created the GameLogic class to manage game state and logic. This class provides methods for pausing and resuming the game and keeps track of the game's pause state.
    
    TODO: Implement additional game logic methods as needed in the future.

--- javaprojects/client/src/main/java/minigames/client/snake/GameLogic.java
+++ javaprojects/client/src/main/java/minigames/client/snake/GameLogic.java
@@ -1,6 +1,41 @@
-// TODO: New home for all of the relevant game logic.
-
 package minigames.client.snake;
 
 public class GameLogic {
+
+    private boolean isGamePaused;
+
+    public GameLogic() {
+        isGamePaused = false;
+    }
+
+    /**
+     * Pause the game.
+     */
+    public void pauseGame() {
+        if (!isGamePaused) {
+            isGamePaused = true;
+            MultimediaManager.playBackgroundSound(GameConstants.GAME_PAUSE_MUSIC);
+        }
+    }
+
+    /**
+     * Resume the game.
+     */
+    public void resumeGame() {
+        if (isGamePaused) {
+            isGamePaused = false;
+            MultimediaManager.playBackgroundSound(GameConstants.GAME_PLAY_MUSIC);
+        }
+    }
+
+    /**
+     * Check if the game is paused.
+     *
+     * @return True if the game is paused, false otherwise.
+     */
+    public boolean isGamePaused() {
+        return isGamePaused;
+    }
+
+    // Add more game logic methods as needed...
 }
