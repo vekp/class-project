0da78117f257893598d1f3f00f8d0ac0f11baf64
Anonymized Author <anon@example.com>
2023-09-19 18:55:37 +1000

    Add ItemType enum for Snake game
    - Introduced an ItemType enum to categorize various fruit items in the game.
    - Enum values include APPLE, CHERRY, ORANGE, and WATERMELON.

---
+++ javaprojects/client/src/main/java/minigames/client/snake/ItemType.java
@@ -0,0 +1,12 @@
+package minigames.client.snake;
+
+/**
+ * Enum to represent different types of items available in the Snake game.
+ * Each item type corresponds to a specific fruit that the snake can consume.
+ */
+public enum ItemType {
+    APPLE,      // Represents an apple item
+    CHERRY,     // Represents a cherry item
+    ORANGE,     // Represents an orange item
+    WATERMELON  // Represents a watermelon item
+}
