24c8e9ffc31888289d4a7994d424c35b225b7f26
Anonymized Author <anon@example.com>
2023-09-20 19:48:37 +1000

    Add GameBoard class for managing the game grid
    - Created the GameBoard class to represent the game grid in the Snake game.
    - Implemented methods for setting and retrieving item types at specific coordinates.
    - Added a method to clear tiles of a specific item type.
    - Initialized the game board with vacant tiles upon creation.

---
+++ javaprojects/client/src/main/java/minigames/client/snake/GameBoard.java
@@ -0,0 +1,47 @@
+package minigames.client.snake;
+
+class GameBoard {
+    private int width, height;
+    private ItemType[][] board;
+
+    public GameBoard(int width, int height) {
+        this.width = width;
+        this.height = height;
+        board = new ItemType[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                board[x][y] = ItemType.VACANT; // Initialize board to be empty
+            }
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public ItemType getItemTypeAt(int x, int y) {
+        return board[x][y];
+    }
+
+    public void setItemTypeAt(int x, int y, ItemType type) {
+        board[x][y] = type;
+    }
+    public ItemType[][] getBoard(){
+        return board;
+    }
+    public void clearTilesOfType(ItemType typeToClear) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (board[x][y] == typeToClear) {
+                    board[x][y] = ItemType.VACANT;
+                }
+            }
+        }
+    }
+
+}
