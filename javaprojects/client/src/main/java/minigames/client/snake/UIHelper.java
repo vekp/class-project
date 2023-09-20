0cc7543bee923136f9979e36eccbc3172fcfa5fa
Anonymized Author <anon@example.com>
2023-09-20 22:30:32 +1000

    Added overloaded version of the return button to play sound on click

--- javaprojects/client/src/main/java/minigames/client/snake/UIHelper.java
+++ javaprojects/client/src/main/java/minigames/client/snake/UIHelper.java
@@ -33,4 +33,27 @@ public class UIHelper {
         container.add(returnButton);
         return returnButton;
     }
+    /**
+     * Sets up a button with custom position, name, and background sound.
+     *
+     * @param panelSwitcher The interface used to switch between panels.
+     * @param container The container (like a JPanel) where the button will be added.
+     * @param x The x-coordinate of the button.
+     * @param y The y-coordinate of the button.
+     * @param buttonName The custom name for the button.
+     * @param soundName The name of the background sound to play when the button is clicked.
+     * @return JButton The created button.
+     */
+    public static JButton setupReturnButton(MainMenuPanel.PanelSwitcher panelSwitcher, JComponent container, int x, int y, String buttonName, String soundName) {
+        ActionListener action = e -> {
+            panelSwitcher.switchToPanel(GameConstants.MAIN_MENU_PANEL);
+            MultimediaManager.playBackgroundSound(soundName);
+        };
+
+        JButton returnButton = ButtonFactory.createButton(buttonName, x, y, GameConstants.BUTTON_WIDTH, GameConstants.BUTTON_HEIGHT, action);
+        container.add(returnButton);
+        return returnButton;
+    }
+
+
 }
