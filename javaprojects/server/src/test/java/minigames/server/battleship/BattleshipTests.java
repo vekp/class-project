d29a13d7532048f799e821efe9b71f2630da8146
Vincent Ekpanyaskun <vekpanya@myune.edu.au>
2023-09-21 22:21:28 +1000

    add unit tests for Board

--- javaprojects/server/src/test/java/minigames/server/battleship/BattleshipTests.java
+++ javaprojects/server/src/test/java/minigames/server/battleship/BattleshipTests.java
@@ -6,16 +6,33 @@ import org.junit.jupiter.api.Test;
  * Ensures that Battleship is performing as expected
  */
 public class BattleshipTests {
+    private final String[] shipClasses = {"Carrier", "Patrol Boat", "Submarine", "Destroyer", "Battleship"};
 
     // Board and Ship Placement
     /** Tests whether a default board is created for the player*/
     @Test
     public void boardDefaultPopulates(){
         // create a board using the default method
+        Board testBoard = new Board(0);
+        Board otherTestBoard = new Board(1);
         // check that the appropriate number of ships are present on the board (maybe have a checker function)
+        assert isBoardValid(testBoard);
+        assert isBoardValid(otherTestBoard);
     }
 
+    /**
+     * Returns whether given Board contains all ships as specified in shipClasses
+     */
+    private boolean isBoardValid (Board board) {
+        for (String shipClass : shipClasses) {
+            if (!board.getVessels().containsKey(shipClass)) {
+                return false;
+            }
+        }
+        return board.getVessels().size() == shipClasses.length;
+    }
 
+    // TODO: implement test after ship placement done
     /** Tests whether ships can be placed on a board*/
     public void shipPlaceable(){
         // create a board with custom placement
@@ -33,10 +50,20 @@ public class BattleshipTests {
 
     // Hit detection
     /** Tests whether hitting an ocean tile will register as a miss, and the cell will be changed as appropriate*/
+    @Test
     public void missDetected(){
         // create an ocean cell
+        Cell testCell = new Cell(0, 0);
+        // assert cell is ocean
+        assert testCell.getCellType().equals(CellType.OCEAN);
+        assert testCell.getCellTypeString().equals("~");
         // shoot at the ocean cell
+        testCell.shoot();
         // assert that the cell is now a "miss" cell
+
+        //TODO : shooting cell type change is handled by BattleshipPlayer, cannot be tested with just a Cell.
+//        assert testCell.getCellType().equals(CellType.MISS);
+//        assert testCell.getCellTypeString().equals(".");
     }
 
     /** Tests whether hitting any of the ship hull types will register a hit, and the cell is changed as appropriate*/
