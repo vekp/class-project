dcc7dae80355c7faf5e93acc81299626636217af
Anonymized Author <anon@example.com>
2023-09-23 15:23:58 +1000

    Commented out TicTacToe - too many errors. Added test file for MongoDB

---
+++ javaprojects/server/src/test/java/minigames/server/survey/SurveyMongoDBTests.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SurveyMongoDBTests {
+    private MongoDB mongoDB;
+
+    @BeforeEach
+    void setUp() {
+        mongoDB = new MongoDB();
+    }
+
+    @Test
+    @ignore
+    void testInsertDocument() {
+    // Create a test document
+    Document testDocument = new Document("name", "TestUser")
+            .append("score", 100);
+
+    // Insert the document into MongoDB
+    JSONArray result = mongoDB.insertDocument("testCollection", testDocument);
+
+    // Check if the insertion was successful
+    assertNotNull(result);
+    assertEquals(1, result.size());
+
+    // Retrieve the inserted document from MongoDB
+    JSONArray retrievedDocuments = mongoDB.getAllDocuments("testCollection");
+
+    // Assert that the retrieved document matches the test document
+    assertNotNull(retrievedDocuments);
+    assertEquals(1, retrievedDocuments.size());
+    JSONObject retrievedDocument = (JSONObject) retrievedDocuments.get(0);
+    assertEquals("TestUser", retrievedDocument.get("name"));
+    assertEquals(100L, retrievedDocument.get("score"));
+}
+
+}
