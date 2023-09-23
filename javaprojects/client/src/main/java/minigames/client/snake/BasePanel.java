2352806845b7ffaf4f08349def3643bb90fb5c1b
Anonymized Author <anon@example.com>
2023-09-23 11:10:30 +1000

    Several updates performed to enhance the base panel class documentation for further clarification

--- javaprojects/client/src/main/java/minigames/client/snake/BasePanel.java
+++ javaprojects/client/src/main/java/minigames/client/snake/BasePanel.java
@@ -6,13 +6,15 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * The BasePanel class serves as the main user interface panel for the Snake game.
+ * The BasePanel class serves as the primary user interface panel for the Snake game.
  * It contains different game panels such as the main menu, game view, information panels, and achievements panel.
  */
 public class BasePanel extends JPanel implements MainMenuPanel.PanelSwitcher {
+    // Panels
     private final JPanel mainPanel = new JPanel(new CardLayout());
     private final JPanel containerPanel = new JPanel(new BorderLayout());
 
+    // Sub-panel instances
     private final MainMenuPanel mainMenuPanel;
     private final GamePlayPanel gameViewPanel;
     private final InformationPanel gameRules;
@@ -28,20 +30,21 @@ public class BasePanel extends JPanel implements MainMenuPanel.PanelSwitcher {
     public BasePanel(MinigameNetworkClient mnClient, Runnable closeGameAction) {
         GameLogic gameLogic = new GameLogic();
 
+        // Initialize sub-panels
         mainMenuPanel = new MainMenuPanel(closeGameAction, this);
-        gameViewPanel = new GamePlayPanel(this, gameLogic); // Pass gameLogic here
+        gameViewPanel = new GamePlayPanel(this, gameLogic);
         gameRules = new InformationPanel(this, GameConstants.GAME_RULES_TITLE, GameConstants.GAME_RULES_MESSAGES);
         aboutMePanel = new InformationPanel(this, GameConstants.ABOUT_ME_TITLE, GameConstants.ABOUT_ME_MESSAGES);
         achievementsPanel = new AchievementsPanel(this);
 
+        // Initialize UI
         initUI();
         configureMainWindow(mnClient);
-        MultimediaManager.playBackgroundSound(GameConstants.MENU_MUSIC);
-
+        MultimediaManager.playBackgroundSound(MusicChoice.MENU_MUSIC);
     }
 
     /**
-     * Initializes the UI by adding sub panels to the main panel and configuring the container panel.
+     * Initializes the user interface by adding sub panels to the main panel and configuring the container panel.
      */
     private void initUI() {
         mainPanel.add(mainMenuPanel, GameConstants.MAIN_MENU_PANEL);
